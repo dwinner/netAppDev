c1d745db420f4a2b2b24ff1424999def0a3e807b
jdeveloper85 <dwinner@inbox.ru>
2016-06-05 02:16:28 +0300

    Compiler platform / Adding property code refactoring

--- Framework Classes Library/Compiler Platform/Playground/ConfigureAwaitTest/Program.cs
+++ Framework Classes Library/Compiler Platform/Playground/ConfigureAwaitTest/Program.cs
@@ -4,73 +4,75 @@ using System.Threading.Tasks;
 namespace ConfigureAwaitTest
 {
     internal class Program
-   {
-      private int _first;
-      private int _second;
-      private int _third;
+    {
+        private int _first;
+        private int _second;
+        private int _third;
 
-      static void Main(string[] args)
-      {            
-      }
-   }
+        public string Name { get; set; }
 
-   // TODO: Add references to the analyzers in the Analyzers section in Solution Explorer after compilation of the solution.
+        static void Main(string[] args)
+        {
+        }
+    }
 
-   internal class Person
-   {
-      int _age;
-      string _name;
-   }
+    // TODO: Add references to the analyzers in the Analyzers section in Solution Explorer after compilation of the solution.
 
-   internal partial class Foo
-   {
-      void If(int x)
-      {
-         if (x > 0)
-            Console.WriteLine(x);
-         else if (x == 0)
-            Console.WriteLine(0);
-         else if (x < 0)
-            Console.WriteLine(-x);
-      }
+    internal class Person
+    {
+        int _age;
+        string _name;
+    }
 
-      void For()
-      {
-         for (var i = 0; i < 10; i++)
-            Console.WriteLine(i);
-      }
+    internal partial class Foo
+    {
+        void If(int x)
+        {
+            if (x > 0)
+                Console.WriteLine(x);
+            else if (x == 0)
+                Console.WriteLine(0);
+            else if (x < 0)
+                Console.WriteLine(-x);
+        }
 
-      void ForEach()
-      {
-         foreach (var x in new[] { 1, 2, 3 })
-            Console.WriteLine(x);
-      }
+        void For()
+        {
+            for (var i = 0; i < 10; i++)
+                Console.WriteLine(i);
+        }
 
-      void While()
-      {
-         while (true)
-            Console.Write('.');
-      }
-   }
+        void ForEach()
+        {
+            foreach (var x in new[] { 1, 2, 3 })
+                Console.WriteLine(x);
+        }
 
-   internal partial class Foo
-   {
-      // NOTE: These methods will only trigger ConfigureAwait requirement violations if the project is compiled as a class library.
-      //       Tweak the project settings accordingly after adding references to the compiled analyzers.
+        void While()
+        {
+            while (true)
+                Console.Write('.');
+        }
+    }
 
-      async Task Bar(Task t)
-      {
-         await t;
-      }
+    internal partial class Foo
+    {
+        // NOTE: These methods will only trigger ConfigureAwait requirement violations if the project is compiled as a class library.
+        //       Tweak the project settings accordingly after adding references to the compiled analyzers.
 
-      async Task<int> Baz(Task<int> t)
-      {
-         return await t;
-      }
+        async Task Bar(Task t)
+        {
+            await t;
+        }
 
-      async Task Qux(Task t)
-      {
-         await t.ConfigureAwait(false);
-      }
-   }
+        async Task<int> Baz(Task<int> t)
+        {
+            return await t;
+        }
+
+        async Task Qux(Task t)
+        {
+            await t.ConfigureAwait(false);
+        }
+    }
 }
---
+++ Framework Classes Library/Compiler Platform/RoadmapAnalyzer/RoadmapAnalyzer/Refactoring/PropertyCodeRefactoringProvider.cs
@@ -0,0 +1,52 @@
+﻿using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace RoadmapAnalyzer.Refactoring
+{
+   [ExportCodeRefactoringProvider(LanguageNames.CSharp, Name = nameof(PropertyCodeRefactoringProvider)), Shared]
+   internal class PropertyCodeRefactoringProvider : CodeRefactoringProvider
+   {
+      public sealed override async Task ComputeRefactoringsAsync(CodeRefactoringContext context)
+      {
+         var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+         var selectedNode = root.FindNode(context.Span);
+         var propertyDecl = selectedNode as PropertyDeclarationSyntax;
+         if (propertyDecl == null || !IsAutoImplementedProperty(propertyDecl))
+         {
+            return;
+         }
+
+         var action = CodeAction.Create("Apply full property",
+            token => ChangeToFullPropertyAsync(context.Document, propertyDecl, token));
+         context.RegisterRefactoring(action);
+      }
+
+      private static bool IsAutoImplementedProperty(BasePropertyDeclarationSyntax propertyDecl)
+      {
+         var accessors = propertyDecl.AccessorList.Accessors;
+         var getter = accessors.FirstOrDefault(ad => ad.Kind() == SyntaxKind.GetAccessorDeclaration);
+         var setter = accessors.FirstOrDefault(ad => ad.Kind() == SyntaxKind.SetAccessorDeclaration);
+
+         return getter != null && setter != null && (getter.Body == null || setter.Body == null);
+      }
+
+      private static async Task<Document> ChangeToFullPropertyAsync(Document document, PropertyDeclarationSyntax propertyDecl,
+         CancellationToken cancellationToken)
+      {
+         var model = await document.GetSemanticModelAsync(cancellationToken).ConfigureAwait(false);
+         var root = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false) as CompilationUnitSyntax;
+         document =
+            document.WithSyntaxRoot(CodeGeneration.ImplementFullProperty(root, model, propertyDecl,
+               document.Project.Solution.Workspace));
+
+         return document;
+      }
+   }
+}
