eb3c2f32d997db9439af622c818a2d4b169c95bd
jdeveloper85 <dwinner@inbox.ru>
2016-06-05 19:22:25 +0300

    Compiler services / Adding syntax tree visualization sample

---
+++ Framework Classes Library/Compiler Platform/GuiSyntaxTree/ViewModels/SyntaxTriviaViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace GuiSyntaxTree.ViewModels
+{
+   public class SyntaxTriviaViewModel
+   {
+      public TriviaKind Kind { get; }
+      public SyntaxTrivia SyntaxTrivia { get; }
+
+      public SyntaxTriviaViewModel(TriviaKind kind, SyntaxTrivia syntaxTrivia)
+      {
+         Kind = kind;
+         SyntaxTrivia = syntaxTrivia;
+      }
+
+      public override string ToString()
+         => $"{Kind}, Start : {SyntaxTrivia.Span.Start}, Lenght: {SyntaxTrivia.Span.Length} : {SyntaxTrivia}";
+   }
+}
