f66bbad26c1916a2b6e34d178d41fc1add2c9150
jdeveloper85 <dwinner@inbox.ru>
2016-06-05 14:15:43 +0300

    Compiler platform / Adding simplifier sample via Workspace API

---
+++ Framework Classes Library/Compiler Platform/SimplificationSample/Program.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.MSBuild;
+using Microsoft.CodeAnalysis.Simplification;
+
+namespace SimplificationSample
+{
+   static class Program
+   {
+      private static readonly Solution Solution =
+         MSBuildWorkspace.Create().OpenSolutionAsync(@"..\..\..\Playground\Playground.sln").Result;
+
+      static void Main()
+      {
+         var project = Solution.Projects.Single(p => p.Name == "ConfigureAwaitTest");
+         var baz = project.Documents.Single(d => d.Name == "Baz.cs");
+
+         Console.WriteLine("Before:");
+         Console.WriteLine();
+         Console.WriteLine(baz.GetSyntaxTreeAsync().Result.GetText());
+
+         Console.WriteLine();
+         Console.WriteLine();
+
+         var oldRoot = baz.GetSyntaxRootAsync().Result;
+         var memberAccesses = oldRoot.DescendantNodes().OfType<CastExpressionSyntax>();
+         var newRoot = oldRoot.ReplaceNodes(memberAccesses, (_, m) => m.WithAdditionalAnnotations(Simplifier.Annotation));
+         var newDocument = baz.WithSyntaxRoot(newRoot);
+
+         // Вызовем сервис упрощения выражений
+         var result = Simplifier.ReduceAsync(newDocument).Result;
+         Console.WriteLine("After:");
+         Console.WriteLine();
+         Console.WriteLine(result.GetSyntaxTreeAsync().Result.GetText());
+      }
+   }
+}
