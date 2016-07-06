6872f3150643feb836d957f447f82bd38d78f84d
dwinner <dwinner@inbox.ru>
2016-07-06 19:00:37 +0300

    Mics

--- ASP.NET/BundlingFeatures/Via.Mvc/Bundling/App_Start/RouteConfig.cs
+++ ASP.NET/BundlingFeatures/Via.Mvc/Bundling/App_Start/RouteConfig.cs
@@ -3,17 +3,15 @@ using System.Web.Routing;
 
 namespace Bundling
 {
-   public class RouteConfig
+   public static class RouteConfig
    {
       public static void RegisterRoutes(RouteCollection routes)
       {
          routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-         routes.MapRoute(
-             name: "Default",
-             url: "{controller}/{action}/{id}",
-             defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-         );
+         routes.MapRoute("Default", "{controller}/{action}/{id}",
+            new {controller = "Home", action = "Index", id = UrlParameter.Optional}
+            );
       }
    }
 }
