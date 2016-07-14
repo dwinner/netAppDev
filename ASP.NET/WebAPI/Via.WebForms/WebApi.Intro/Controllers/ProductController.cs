63e6e923be578bffdf2b6efb1c73c7e3157bca73
dwinner <dwinner@inbox.ru>
2016-07-14 15:07:35 +0300

    Misc

--- ASP.NET/WebAPI/Via.WebForms/WebApi.Intro/Controllers/ProductController.cs
+++ ASP.NET/WebAPI/Via.WebForms/WebApi.Intro/Controllers/ProductController.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -7,40 +6,31 @@ using System.Web.Http;
 using System.Web.ModelBinding;
 using AutoMapper;
 using WebApi.Intro.Models;
-// ReSharper disable MemberCanBePrivate.Global
 
 namespace WebApi.Intro.Controllers
 {
-   [SuppressMessage("ReSharper", "UnusedMember.Global")]
    public class ProductController : ApiController
    {
       private readonly Repository _repo = new Repository();
 
       // GET api/<controller>
       public IEnumerable<ProductView> Get()
-      {
-         return _repo.Products.Select(Mapper.Map<ProductView>);
-      }
+         => _repo.Products.Select(Mapper.Map<ProductView>);
 
       // GET api/<controller>?categoryFilter=filterValue
       public IEnumerable<ProductView> Get([Form] string categoryFilter)
-      {
-         return categoryFilter == null || categoryFilter == "All"
-            ? Get()
-            : _repo.Products.Where(product => product.Category == categoryFilter).Select(Mapper.Map<ProductView>);
-      }
+         =>
+            categoryFilter == null || categoryFilter == "All"
+               ? Get()
+               : _repo.Products.Where(product => product.Category == categoryFilter).Select(Mapper.Map<ProductView>);
 
       // GET api/<controller>/5
       public ProductView Get(int id)
-      {
-         return Mapper.Map<ProductView>(_repo.Products.FirstOrDefault(product => product.ProductId == id));
-      }      
+         => Mapper.Map<ProductView>(_repo.Products.FirstOrDefault(product => product.ProductId == id));
 
       // POST api/<controller>
       public void Post([FromBody] ProductView value)
-      {
-         _repo.Add(Mapper.Map<Product>(value));
-      }
+         => _repo.Add(Mapper.Map<Product>(value));
 
       // PUT api/<controller>/5
       public HttpResponseMessage Put(int id, [FromBody] ProductView value)
@@ -58,7 +48,8 @@ namespace WebApi.Intro.Controllers
          }
          else
          {
-            var errors= (ModelState.SelectMany(state => state.Value.Errors, (state, error) => error.ErrorMessage)).ToList();
+            var errors =
+               ModelState.SelectMany(state => state.Value.Errors, (state, error) => error.ErrorMessage).ToList();
             responseMessage = Request.CreateResponse(HttpStatusCode.BadRequest, errors);
          }
 
