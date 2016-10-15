87929bc1d34e9a987f11338bb99cce8c7753aa3f
dwinner <dwinner@inbox.ru>
2016-10-15 12:52:41 +0300

    Adding untracked projects

---
+++ Metaprogramming/Customers.Lib/CustomerHardCoded.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace Customers.Lib
+{
+   public sealed class CustomerHardCoded : Customer
+   {
+      public override string ToString()
+         => new StringBuilder()
+            .Append("Age: ").Append(Age)
+            .Append(Constants.Separator)
+            .Append("Id: ").Append(Id)
+            .Append(Constants.Separator)
+            .Append("FirstName: ").Append(FirstName)
+            .Append(Constants.Separator)
+            .Append("LastName: ").Append(LastName).ToString();
+   }
+}
