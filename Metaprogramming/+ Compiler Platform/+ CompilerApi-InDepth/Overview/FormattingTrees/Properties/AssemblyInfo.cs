5363b280b76604c608f7ae230bbb01d2f3bf5ca1
dwinner <dwinner@inbox.ru>
2016-11-09 14:19:26 +0300

    Code has been completed for Overview

--- Metaprogramming/+ Compiler Platform/+ CompilerApi-InDepth/Overview/FormattingTrees/Properties/AssemblyInfo.cs
+++ Metaprogramming/+ Compiler Platform/+ CompilerApi-InDepth/Overview/FormattingTrees/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ModifyingTrees")]
+[assembly: AssemblyTitle("FormattingTrees")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ModifyingTrees")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyProduct("FormattingTrees")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -18,9 +17,6 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a0458ea6-dc14-4d1e-af27-ec8a25dc3f10")]
 
 // Version information for an assembly consists of the following four values:
 //
--- Metaprogramming/+ Compiler Platform/+ CompilerApi-InDepth/Overview/WalkingTrees/Properties/AssemblyInfo.cs
+++ Metaprogramming/+ Compiler Platform/+ CompilerApi-InDepth/Overview/WalkingTrees/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CompilerAPIMemberCount")]
+[assembly: AssemblyTitle("WalkingTrees")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CompilerAPIMemberCount")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyProduct("WalkingTrees")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -18,9 +17,6 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2a68265b-e135-4f3f-858e-c44843372771")]
 
 // Version information for an assembly consists of the following four values:
 //
