8580e075def690b3d8326d3cadd45596bb9dbb35
dwinner <dwinner@inbox.ru>
2016-11-09 16:06:36 +0300

    Restructured for Compiler API

--- Metaprogramming/+ Compiler Platform/Working With Symbols/SymbolVisitorSample/Program.cs
+++ Metaprogramming/+ Compiler Platform/Working With Symbols/SymbolVisitorSample/Program.cs
@@ -7,7 +7,7 @@ namespace SymbolVisitorSample
 {
    internal static class Program
    {
-      private static readonly SyntaxTree SourceTree = CSharpSyntaxTree.ParseText(@"
+      private static readonly SyntaxTree _SourceTree = CSharpSyntaxTree.ParseText(@"
 class MyClass
 {
     class Nested
@@ -21,7 +21,7 @@ class MyClass
       private static void Main()
       {
          var mscorlib = MetadataReference.CreateFromFile(typeof(object).Assembly.Location);
-         var compilation = CSharpCompilation.Create("MyCompilation", new[] { SourceTree }, new[] { mscorlib });
+         var compilation = CSharpCompilation.Create("MyCompilation", new[] { _SourceTree }, new[] { mscorlib });
 
          //SymbolVisitor visitor = new NamedTypeVisitor();
          //visitor.Visit(compilation.GlobalNamespace);
