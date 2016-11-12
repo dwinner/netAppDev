9712aa2448bd3887e41bb208d76d85cb24ebce4d
bboytronik@gmail.com <bboytronik@gmail.com>
2016-11-12 13:17:18 +0300

    Comment remover / WIP

--- Metaprogramming/+ Compiler Platform/Workspaces/CommentRemover/CommentRemover.IntegrationApp/Properties/AssemblyInfo.cs
+++ Metaprogramming/+ Compiler Platform/Workspaces/CommentRemover/CommentRemover.IntegrationApp/Properties/AssemblyInfo.cs
@@ -1,15 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-[assembly: AssemblyTitle("CommentRemover.ConsoleApplication.IntegrationTests")]
+[assembly: AssemblyTitle("CommentRemover.IntegrationApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CommentRemover.ConsoleApplication.IntegrationTests")]
+[assembly: AssemblyProduct("CommentRemover.IntegrationApp")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: Guid("1b737466-f7a7-4b7d-9a71-d23e62e50388")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
