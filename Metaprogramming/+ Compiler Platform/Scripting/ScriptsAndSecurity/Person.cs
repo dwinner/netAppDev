a833396fd1ad5d35fe3ed4693e6c0595dc6bd2b0
bboytronik@gmail.com <bboytronik@gmail.com>
2016-11-24 21:15:01 +0300

    Finished sample apps about scripting api and Compiler API in general

---
+++ Metaprogramming/+ Compiler Platform/Scripting/ScriptsAndSecurity/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace ScriptsAndSecurity
+{
+   public sealed class Person
+   {
+      public Person(string name, uint age)
+      {
+         Name = name;
+         Age = age;
+      }
+
+      public uint Age { get; set; }
+      public string Name { get; set; }
+
+      public void Save()
+      {
+      }
+   }
+}
