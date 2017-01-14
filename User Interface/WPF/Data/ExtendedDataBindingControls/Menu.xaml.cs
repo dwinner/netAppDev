0651d192aaa44db3bdf6db87603b28e1c2a11a3b
jdeveloper85 <dwinner@inbox.ru>
2017-01-14 12:46:53 +0300

    Extended data binding / Refactoring the common menu

--- User Interface/WPF/Data/ExtendedDataBindingControls/Menu.xaml.cs
+++ User Interface/WPF/Data/ExtendedDataBindingControls/Menu.xaml.cs
@@ -1,18 +1,10 @@
-using System;
-using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 
-
 namespace DataBinding
 {
-   /// <summary>
-   /// Interaction logic for Window1.xaml
-   /// </summary>
-
-   public partial class Menu : Window
+   public partial class Menu
    {
-
       public Menu()
       {
          InitializeComponent();
@@ -21,17 +13,16 @@ namespace DataBinding
       private void ButtonClick(object sender, RoutedEventArgs e)
       {
          // Get the current button.
-         Button cmd = (Button)e.OriginalSource;
+         var cmd = (Button) e.OriginalSource;
 
          // Create an instance of the window named
          // by the current button.
-         Type type = this.GetType();
-         Assembly assembly = type.Assembly;
-         Window win = (Window)assembly.CreateInstance(
-             type.Namespace + "." + cmd.Content);
+         var type = GetType();
+         var assembly = type.Assembly;
+         var win = (Window) assembly.CreateInstance($"{type.Namespace}.{cmd.Content}");
 
          // Show the window.
-         win.ShowDialog();
+         win?.ShowDialog();
       }
    }
 }
