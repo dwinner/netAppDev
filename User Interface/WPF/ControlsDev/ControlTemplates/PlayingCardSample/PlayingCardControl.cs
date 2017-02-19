2fbf21ec4a411f70ca1e951bf7065345d79b8019
bboytronik@gmail.com <bboytronik@gmail.com>
2017-02-19 11:09:18 +0300

    Controls Dev

---
+++ User Interface/WPF/ControlsDev/ControlTemplates/PlayingCardSample/PlayingCardControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace PlayingCardSample
+{
+   public sealed class PlayingCardControl : ToggleButton
+   {
+      public static readonly DependencyProperty FaceProperty = DependencyProperty.Register(
+         "Face", typeof(string), typeof(PlayingCardControl), new PropertyMetadata(default(string)));
+
+      static PlayingCardControl()
+      {
+         DefaultStyleKeyProperty.OverrideMetadata(typeof(PlayingCardControl),
+            new FrameworkPropertyMetadata(typeof(PlayingCardControl)));
+      }
+
+      public string Face
+      {
+         get { return (string) GetValue(FaceProperty); }
+         set { SetValue(FaceProperty, value); }
+      }
+   }
+}
