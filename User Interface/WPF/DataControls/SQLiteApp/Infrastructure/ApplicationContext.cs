e9a9f2c5ccd4eb2fc5f11d10b4c9a18ca9b90b5b
bboytronik@gmail.com <bboytronik@gmail.com>
2017-02-24 20:40:47 +0300

    Adding SQLite sample

---
+++ User Interface/WPF/DataControls/SQLiteApp/Infrastructure/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using SQLiteApp.Models;
+
+namespace SQLiteApp.Infrastructure
+{
+	public sealed class ApplicationContext : DbContext
+	{
+		public ApplicationContext() : base("DefaultConnection")
+		{
+		}
+
+		public DbSet<Phone> Phones { get; set; }
+	}
+}
---
+++ User Interface/WPF/DataControls/SQLiteApp/PhoneWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using SQLiteApp.Models;
+
+namespace SQLiteApp
+{
+	public partial class PhoneWindow
+	{
+		public PhoneWindow(Phone phone)
+		{
+			InitializeComponent();
+
+			Phone = phone;
+			DataContext = Phone;
+		}
+
+		public Phone Phone { get; }
+
+		private void OnAccept(object sender, RoutedEventArgs e)
+		{
+			DialogResult = true;
+		}
+	}
+}
