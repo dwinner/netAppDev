9ce133add86c8655dd24bc495e620fc1432cf191
bboytronik@gmail.com <bboytronik@gmail.com>
2017-02-28 00:27:38 +0300

    Interop with Windows forms

---
+++ User Interface/WPF/Interoperability/WinForms/HostingWinFormsViaCode/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+
+namespace HostingWinFormsViaCode
+{
+	public partial class MainWindow
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void OnLoaded(object sender, RoutedEventArgs e)
+		{
+			// Create the host and the PropertyGrid control
+			var propertyGrid = new PropertyGrid();
+			var winFormsHost = new WindowsFormsHost {Child = propertyGrid};
+
+			// Add the PropertyGrid to the host, and the host to the WPF Grid
+			TopLevelGrid.Children.Add(winFormsHost);
+			propertyGrid.SelectedObject = this;
+		}
+	}
+}
