0aa8b9f22298f852baead1b6a8dd858645c1013e
bboytronik@gmail.com <bboytronik@gmail.com>
2017-03-01 22:42:50 +0300

    Documents in WPF / WIP

--- User Interface/WPF/Documents/FlowContent.xaml.cs
+++ User Interface/WPF/Documents/FlowContent.xaml.cs
@@ -3,47 +3,38 @@ using System.Windows.Documents;
 
 namespace Documents
 {
-   /// <summary>
-   /// Interaction logic for FlowContent.xaml
-   /// </summary>
-
-   public partial class FlowContent : System.Windows.Window
+   public partial class FlowContent
    {
-
       public FlowContent()
       {
          InitializeComponent();
-
       }
 
-      private void cmdCreateDynamicDocument_Click(object sender, RoutedEventArgs e)
+      private void OnCreateDynamicDocument(object sender, RoutedEventArgs e)
       {
          // Create first part of sentence.
-         Run runFirst = new Run();
-         runFirst.Text = "Hello world of ";
+         var runFirst = new Run {Text = "Hello world of "};
 
          // Create bolded text.
-         Bold bold = new Bold();
-         Run runBold = new Run();
-         runBold.Text = "dynamically generated";
+         var bold = new Bold();
+         var runBold = new Run {Text = "dynamically generated"};
          bold.Inlines.Add(runBold);
 
          // Create last part of sentence.
-         Run runLast = new Run();
-         runLast.Text = " documents";
+         var runLast = new Run {Text = " documents"};
 
          // Add three parts of sentence to a paragraph, in order.
-         Paragraph paragraph = new Paragraph();
+         var paragraph = new Paragraph();
          paragraph.Inlines.Add(runFirst);
          paragraph.Inlines.Add(bold);
          paragraph.Inlines.Add(runLast);
 
          // Create a document and add this paragraph.
-         FlowDocument document = new FlowDocument();
+         var document = new FlowDocument();
          document.Blocks.Add(paragraph);
 
          // Show the document.
-         docViewer.Document = document;
+         DocViewer.Document = document;
       }
    }
 }
