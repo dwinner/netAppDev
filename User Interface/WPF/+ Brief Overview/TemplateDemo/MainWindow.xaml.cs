fd8e1670f9230b5bab479bbe7a5c8b52b70bc6ba
bboytronik@gmail.com <bboytronik@gmail.com>
2017-03-25 02:34:57 +0300

    WPF brief overview / WIP

---
+++ User Interface/WPF/+ Brief Overview/TemplateDemo/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace TemplateDemo
+{
+   public partial class MainWindow
+   {
+      public MainWindow()
+      {
+         InitializeComponent();
+      }
+
+      private void OnStyledButton(object sender, RoutedEventArgs e)
+      {
+         new StyledButtonWindow().Show();
+      }
+
+      private void OnStyledListBox(object sender, RoutedEventArgs e)
+      {
+         new StyledListBoxWindow().Show();
+      }
+   }
+}
---
+++ User Interface/WPF/+ Brief Overview/TriggerDemo/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace TriggerDemo
+{
+   public partial class MainWindow
+   {
+      public MainWindow()
+      {
+         InitializeComponent();
+      }
+
+      private void OnPropertyTrigger(object sender, RoutedEventArgs e)
+      {
+         new PropertyTriggerWindow().Show();
+      }
+
+      private void OnMultiTrigger(object sender, RoutedEventArgs e)
+      {
+         new MultiTriggerWindow().Show();
+      }
+
+      private void OnDataTrigger(object sender, RoutedEventArgs e)
+      {
+         new DataTriggerWindow().Show();
+      }
+   }
+}
