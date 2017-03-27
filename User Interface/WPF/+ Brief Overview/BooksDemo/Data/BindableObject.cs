e6d9dc008a881a1e39c4fe47b45f821cc6b61fa8
bboytronik@gmail.com <bboytronik@gmail.com>
2017-03-27 13:51:21 +0300

    WPF overview / Adding BooksDemo App

---
+++ User Interface/WPF/+ Brief Overview/BooksDemo/Data/BindableObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BooksDemo.Data
+{
+   public abstract class BindableObject : INotifyPropertyChanged
+   {
+      public event PropertyChangedEventHandler PropertyChanged;
+
+      private void OnPropertyChanged(string propertyName = null)
+         => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+      protected void SetProperty<T>(ref T anItem, T aNewValue, [CallerMemberName] string aPropertyName = null)
+      {
+         if (!EqualityComparer<T>.Default.Equals(anItem, aNewValue))
+         {
+            anItem = aNewValue;
+            OnPropertyChanged(aPropertyName);
+         }
+      }
+   }
+}
