b53dd22bfc7b280a81fbd0cfb631a5bace11c4ab
bboytronik@gmail.com <bboytronik@gmail.com>
2017-04-12 20:19:58 +0300

    Misc

--- User Interface/WPF/Controls Development/CustomControls/CustomControls/CustomDrawnElement.cs
+++ User Interface/WPF/Controls Development/CustomControls/CustomControls/CustomDrawnElement.cs
@@ -4,65 +4,60 @@ using System.Windows.Media;
 
 namespace CustomControls
 {
-   /// <summary>
-   ///    Элемент управления, который рисует себя сам
-   /// </summary>
-   public class CustomDrawnElement : FrameworkElement
-   {
-      public static readonly DependencyProperty BackgroundColorProperty;
+	public class CustomDrawnElement : FrameworkElement
+	{
+		public static readonly DependencyProperty BackgroundColorProperty;
 
-      static CustomDrawnElement()
-      {
-         var metadata = new FrameworkPropertyMetadata(Colors.Yellow) { AffectsRender = true };
-         BackgroundColorProperty = DependencyProperty.Register("BackgroundColor",
-            typeof(Color), typeof(CustomDrawnElement), metadata);
-      }
+		static CustomDrawnElement()
+		{
+			var metadata = new FrameworkPropertyMetadata(Colors.Yellow) {AffectsRender = true};
+			BackgroundColorProperty = DependencyProperty.Register(nameof(BackgroundColor),
+				typeof(Color), typeof(CustomDrawnElement), metadata);
+		}
 
-      public Color BackgroundColor
-      {
-         get { return (Color)GetValue(BackgroundColorProperty); }
-         set { SetValue(BackgroundColorProperty, value); }
-      }
+		public Color BackgroundColor
+		{
+			get { return (Color) GetValue(BackgroundColorProperty); }
+			set { SetValue(BackgroundColorProperty, value); }
+		}
 
-      private Brush GetForegroundBrush()
-      {
-         if (!IsMouseOver)
-         {
-            return new SolidColorBrush(BackgroundColor);
-         }
-         
-         var absoluteGradientOrigin = Mouse.GetPosition(this);
-         var relativeGradientOrigin = new Point(
-            absoluteGradientOrigin.X / ActualWidth, absoluteGradientOrigin.Y / ActualHeight);
+		private Brush GetForegroundBrush()
+		{
+			if (!IsMouseOver)
+				return new SolidColorBrush(BackgroundColor);
 
-         var brush = new RadialGradientBrush(Colors.White, BackgroundColor)
-         {
-            GradientOrigin = relativeGradientOrigin,
-            Center = relativeGradientOrigin
-         };
-         brush.Freeze();
+			var absoluteGradientOrigin = Mouse.GetPosition(this);
+			var relativeGradientOrigin = new Point(
+				absoluteGradientOrigin.X / ActualWidth, absoluteGradientOrigin.Y / ActualHeight);
 
-         return brush;
-      }
+			var brush = new RadialGradientBrush(Colors.White, BackgroundColor)
+			{
+				GradientOrigin = relativeGradientOrigin,
+				Center = relativeGradientOrigin
+			};
+			brush.Freeze();
 
-      protected override void OnRender(DrawingContext drawingContext)
-      {
-         base.OnRender(drawingContext);
+			return brush;
+		}
 
-         var bounds = new Rect(0, 0, ActualWidth, ActualHeight);
-         drawingContext.DrawRectangle(GetForegroundBrush(), null, bounds);
-      }
+		protected override void OnRender(DrawingContext drawingContext)
+		{
+			base.OnRender(drawingContext);
 
-      protected override void OnMouseMove(MouseEventArgs e)
-      {
-         base.OnMouseMove(e);
-         InvalidateVisual();
-      }
+			var bounds = new Rect(0, 0, ActualWidth, ActualHeight);
+			drawingContext.DrawRectangle(GetForegroundBrush(), null, bounds);
+		}
 
-      protected override void OnMouseLeave(MouseEventArgs e)
-      {
-         base.OnMouseLeave(e);
-         InvalidateVisual();
-      }
-   }
+		protected override void OnMouseMove(MouseEventArgs e)
+		{
+			base.OnMouseMove(e);
+			InvalidateVisual();
+		}
+
+		protected override void OnMouseLeave(MouseEventArgs e)
+		{
+			base.OnMouseLeave(e);
+			InvalidateVisual();
+		}
+	}
 }
