3c3813c6158d7007bc2fd908b44e5deb467a5519
bboytronik@gmail.com <bboytronik@gmail.com>
2017-08-27 21:01:52 +0300

    Misc

---
+++ IoC Features/Unity Application Block/InjectedLoggerApp/FileWriter.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace InjectedLoggerApp
+{
+	public sealed class FileWriter : IWriter
+	{
+		public void Write(string aMessage)
+		{
+			using (var streamWriter = new StreamWriter("c:\\TestUnity.txt", true))
+			{
+				streamWriter.WriteLine(aMessage);
+			}
+		}
+	}
+}
