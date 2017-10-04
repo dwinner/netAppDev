94d12b73f637675cdd6e22fb349ef59967076ef3
DeVi <dwinner@inbox.ru>
2017-10-04 18:07:57 +0300

    Advanced TPL dataflow / WIP

---
+++ Framework Classes Library/Concurrency/TPL. Dataflow (Advanced)/QuantityBatcher/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks.Dataflow;
+
+namespace QuantityBatcher
+{
+   internal static class Program
+   {
+      private static void Main()
+      {
+         const int batchSize = 100;
+         var batcher = new BatchBlock<int>(batchSize);
+
+         var averager = new ActionBlock<int[]>(intValues =>
+            Console.WriteLine("{0} reduced to {1}", intValues.Length, intValues.Average()));
+
+         batcher.LinkTo(averager);
+
+         Console.WriteLine("Press 'Q' to quit");
+         var rnd = new Random();
+         bool quit;
+         do
+         {
+            Thread.Sleep(TimeSpan.FromMilliseconds(10));
+            batcher.Post(rnd.Next(1, 100));
+            quit = Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Q;
+         } while (!quit);
+
+         batcher.Complete();
+         batcher.Completion.Wait();
+      }
+   }
+}
