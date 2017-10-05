a103e94b9d04d9254d8ae48f424964230c867736
bboytronik@gmail.com <bboytronik@gmail.com>
2017-10-05 20:37:47 +0300

    misc

---
+++ Framework Classes Library/Concurrency/TPL. Dataflow (Advanced)/Joining/Chef.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Joining
+{
+   public class Chef
+   {
+      private const int DefaultFoodItemCount = 4;
+      private readonly Restaurant _restaurant;
+
+      public Chef(Restaurant restaurant) => _restaurant = restaurant;
+
+      public async Task MakeFoodAsync()
+      {
+         Console.WriteLine("Chef making food");
+         var knife = await _restaurant.Knife.ReceiveAsync().ConfigureAwait(false);
+         for (var nFoodItem = 0; nFoodItem < DefaultFoodItemCount; nFoodItem++)
+            _restaurant.Food.Post(new Food());
+
+         Console.WriteLine("Chef made food...");
+         _restaurant.Knife.Post(knife);
+      }
+   }
+}
