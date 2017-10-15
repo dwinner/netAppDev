ef37791978788000d185ab4bb526d0a6ac19f113
bboytronik@gmail.com <bboytronik@gmail.com>
2017-10-15 20:30:15 +0300

    Advanced TPL dataflow / WIP

---
+++ Framework Classes Library/Concurrency/TPL. Dataflow (Advanced)/SingleActionBlockLoadBalance/TaskExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SingleActionBlockLoadBalance
+{
+   public static class TaskExtensions
+   {
+      public static Task[] ContinueWithMany<T>(this Task<T> @this,
+         params Func<Task<T>, Task>[] continuations)
+      {
+         var results = new Task[continuations.Length];
+         for (var i = 0; i < continuations.Length; i++)
+            results[i] = continuations[i](@this);
+
+         return results;
+      }
+   }
+}
