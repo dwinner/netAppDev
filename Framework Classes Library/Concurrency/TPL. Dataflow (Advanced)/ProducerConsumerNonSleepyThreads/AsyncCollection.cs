267c1e0e61caa574104a5f02e89127397df753ee
bboytronik@gmail.com <bboytronik@gmail.com>
2017-10-15 14:09:49 +0300

    Advanced TPL dataflow / WIP

---
+++ Framework Classes Library/Concurrency/TPL. Dataflow (Advanced)/ProducerConsumerNonSleepyThreads/AsyncCollection.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Concurrent;
+using System.Threading.Tasks;
+// ReSharper disable InconsistentlySynchronizedField
+
+namespace ProducerConsumerNonSleepyThreads
+{
+   public class AsyncCollection<T>
+   {
+      private readonly IProducerConsumerCollection<T> _collection;
+      private readonly ConcurrentQueue<TaskCompletionSource<T>> _consumers;
+
+      public AsyncCollection(IProducerConsumerCollection<T> collection)
+      {
+         _collection = collection;
+         _consumers = new ConcurrentQueue<TaskCompletionSource<T>>();
+      }
+
+      public Task<T> TakeAsync()
+      {
+         var taskCompletionSource = new TaskCompletionSource<T>();
+         if (!TryGetItem(taskCompletionSource))
+            lock (_collection)
+            {
+               if (!TryGetItem(taskCompletionSource))
+                  _consumers.Enqueue(taskCompletionSource);
+            }
+
+         return taskCompletionSource.Task;
+      }
+
+      public void Add(T item)
+      {
+         if (!TryWaitingConsumer(item))
+            lock (_collection)
+            {
+               if (!TryWaitingConsumer(item))
+                  _collection.TryAdd(item);
+            }
+      }
+
+      private bool TryWaitingConsumer(T item)
+      {
+         if (_consumers.TryDequeue(out var taskCompletion))
+         {
+            Task.Run(() => taskCompletion.SetResult(item));
+            return true;
+         }
+
+         return false;
+      }
+
+      private bool TryGetItem(TaskCompletionSource<T> taskCompletionSource)
+      {
+         if (_collection.TryTake(out var item))
+         {
+            taskCompletionSource.SetResult(item);
+            return true;
+         }
+
+         return false;
+      }
+   }
+}
---
+++ Framework Classes Library/Concurrency/TPL. Dataflow (Advanced)/ProducerConsumerNonSleepyThreads/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace ProducerConsumerNonSleepyThreads
+{
+   internal static class Program
+   {
+      private static void Main()
+      {
+         var dcq = new LazyProducerConsumerQueue<int>(3, i =>
+         {
+            Console.WriteLine("{0}:Processing...", Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(i);
+            Console.WriteLine("{0}:Done", Thread.CurrentThread.ManagedThreadId);
+         });
+
+         dcq.Enqueue(1000);
+         Thread.Sleep(2000);
+         dcq.Enqueue(200);
+         Thread.Sleep(1000);
+
+         var rnd = new Random();
+         for (var i = 0; i < 10; i++)
+            dcq.Enqueue(rnd.Next(1000, 5000));
+         Console.ReadLine();
+         dcq.Enqueue(2000);
+         Thread.Sleep(2000);
+         dcq.Enqueue(1000);
+
+         Console.ReadLine();
+      }
+   }
+}
