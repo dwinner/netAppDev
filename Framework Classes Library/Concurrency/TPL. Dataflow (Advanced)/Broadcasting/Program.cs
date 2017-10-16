2569162e257882fae90dffff2cc9324a990c7352
DeVi <dwinner@inbox.ru>
2017-10-16 13:38:41 +0300

    Advanced TPL dataflow / completed

---
+++ Framework Classes Library/Concurrency/TPL. Dataflow (Advanced)/Broadcasting/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks.Dataflow;
+
+namespace Broadcasting
+{
+   internal static class Program
+   {
+      private static void Main()
+      {
+         void ConsumerOne(int obj)
+         {
+            Console.WriteLine("Consumer one {0}", obj);
+            Thread.Sleep(TimeSpan.FromMilliseconds(100));
+         }
+
+         void ConsumerTwo(int obj)
+         {
+            Console.WriteLine("Consumer two {0}", obj);
+            Thread.Sleep(TimeSpan.FromMilliseconds(60));
+         }
+
+         var nonGreedy = new ExecutionDataflowBlockOptions {BoundedCapacity = 1};
+         var greedy = new ExecutionDataflowBlockOptions();
+
+         var source = new BroadcastBlock<int>(i => i);
+
+         var consumeOne = new ActionBlock<int>(i => ConsumerOne(i), nonGreedy);
+         var consumeTwo = new ActionBlock<int>(i => ConsumerTwo(i), greedy);
+
+         source.LinkTo(consumeOne);
+         source.LinkTo(consumeTwo);
+
+         for (var i = 0; i < 10; i++)
+         {
+            source.Post(i);
+            Thread.Sleep(50);
+         }
+
+         Console.ReadLine();
+      }
+   }
+}
---
+++ Framework Classes Library/Concurrency/TPL. Dataflow (Advanced)/NonGreedyLinkingBlockSample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks.Dataflow;
+
+namespace NonGreedyLinkingBlockSample
+{
+   internal static class Program
+   {
+      private static void Main()
+      {
+         //var greedy = new ExecutionDataflowBlockOptions();
+         var nonGreedy = new ExecutionDataflowBlockOptions {BoundedCapacity = 1};
+
+         var options = nonGreedy;
+
+         var firstBlock = new ActionBlock<int>(i => Do(i, 1, 2), options);
+         var secondBlock = new ActionBlock<int>(i => Do(i, 2, 1), options);
+         var thirdBlock = new ActionBlock<int>(i => Do(i, 3, 2), options);
+
+         var transform = new TransformBlock<int, int>(i => i * 2);
+
+         transform.LinkTo(firstBlock);
+         transform.LinkTo(secondBlock);
+         transform.LinkTo(thirdBlock);
+
+         for (var i = 0; i <= 10; i++)
+            transform.Post(i);
+
+         Console.ReadLine();
+      }
+
+      private static void Do(int workItem, int nWorker, int busyTimeInSeconds)
+      {
+         Console.WriteLine("Worker {0} Busy processing {1}", nWorker, workItem);
+         Thread.Sleep(busyTimeInSeconds * 1000);
+         Console.WriteLine("Worker {0} Done", nWorker);
+      }
+   }
+}
