4a62607210a85608a250040bc463de9b228ae303
bboytronik@gmail.com <bboytronik@gmail.com>
2017-10-26 21:34:30 +0300

    T4 samples / WIP

--- Metaprogramming/T4-Generation-Samples/BasicIndentationSample/Properties/AssemblyInfo.cs
+++ Metaprogramming/T4-Generation-Samples/BasicIndentationSample/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GenerateDataClassTemplate")]
+[assembly: AssemblyTitle("BasicIndentationSample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GenerateDataClassTemplate")]
+[assembly: AssemblyProduct("BasicIndentationSample")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f90328e2-6211-4b65-a284-9b7dc081f2ca")]
+[assembly: Guid("3ad05b33-abaf-4276-96cc-5337c3086d60")]
 
 // Version information for an assembly consists of the following four values:
 //
--- Metaprogramming/T4-Generation-Samples/ClassFeatureBlockSample/Properties/AssemblyInfo.cs
+++ Metaprogramming/T4-Generation-Samples/ClassFeatureBlockSample/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GenerateDataClassTemplate")]
+[assembly: AssemblyTitle("ClassFeatureBlockSample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GenerateDataClassTemplate")]
+[assembly: AssemblyProduct("ClassFeatureBlockSample")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f90328e2-6211-4b65-a284-9b7dc081f2ca")]
+[assembly: Guid("cac50dc0-c640-4e40-82b0-a7250fc4e1c3")]
 
 // Version information for an assembly consists of the following four values:
 //
