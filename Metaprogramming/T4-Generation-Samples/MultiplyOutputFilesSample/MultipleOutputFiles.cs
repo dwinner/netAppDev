267f8371179846d7df9649102e490506645d45e5
DeVi <dwinner@inbox.ru>
2017-11-01 18:35:04 +0300

    Added more T4 samples

---
+++ Metaprogramming/T4-Generation-Samples/MultiplyOutputFilesSample/MultipleOutputFiles.cs
@@ -0,0 +1,16 @@
+﻿
+// this output code will be written to MultipleOutputFiles.cs
+// Generated helper templates
+// T4-Generation-Samples\MultiplyOutputFilesSample\Test\MultipleOutputFiles.txt4
+// Generated items
+// T4-Generation-Samples\MultiplyOutputFilesSample\Test\SubFile.txt
+// T4-Generation-Samples\MultiplyOutputFilesSample\Outputfile9.txt
+// T4-Generation-Samples\MultiplyOutputFilesSample\Outputfile8.txt
+// T4-Generation-Samples\MultiplyOutputFilesSample\Outputfile7.txt
+// T4-Generation-Samples\MultiplyOutputFilesSample\Outputfile6.txt
+// T4-Generation-Samples\MultiplyOutputFilesSample\Outputfile5.txt
+// T4-Generation-Samples\MultiplyOutputFilesSample\Outputfile4.txt
+// T4-Generation-Samples\MultiplyOutputFilesSample\Outputfile3.txt
+// T4-Generation-Samples\MultiplyOutputFilesSample\Outputfile2.txt
+// T4-Generation-Samples\MultiplyOutputFilesSample\Outputfile1.txt
+// T4-Generation-Samples\MultiplyOutputFilesSample\Outputfile0.txt
