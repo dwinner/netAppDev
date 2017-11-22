523d446060156ce92c311a1298e1cf41ccb353a4
bboytronik@gmail.com <bboytronik@gmail.com>
2017-11-22 18:11:43 +0300

    Added adapted factory method example

---
+++ Object Oriented Pro/Object Generators/AdaptFactory/Program.cs
@@ -0,0 +1,21 @@
+﻿/**
+ * Adapted factory method for transfering Comparison To Comparer
+ */
+
+using System;
+using System.Collections.Generic;
+using static AdaptFactory.ComparerFactory;
+
+namespace AdaptFactory
+{
+   internal static class Program
+   {
+      private static void Main()
+      {
+         ISet<Employee> set = new SortedSet<Employee>(Create<Employee>((x, y) => x.Id.CompareTo(y.Id)));
+         set.Add(new Employee {Id = 1, Name = "Den"});
+         foreach (var employee in set)
+            Console.WriteLine(employee);
+      }
+   }
+}
