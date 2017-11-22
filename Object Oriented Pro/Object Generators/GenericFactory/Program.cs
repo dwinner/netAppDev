5916e942509c83be0e880796ca73bd550cff193f
bboytronik@gmail.com <bboytronik@gmail.com>
2017-11-22 18:45:57 +0300

    Added The generic factories sample

---
+++ Object Oriented Pro/Object Generators/GenericFactory/Program.cs
@@ -0,0 +1,17 @@
+﻿/**
+ * The generic factories
+ */
+
+using System;
+
+namespace GenericFactory
+{
+   internal static class Program
+   {
+      private static void Main()
+      {
+         var concreteProduct = ProductFactory.Create<ConcreteProduct>();
+         Console.WriteLine(concreteProduct);
+      }
+   }
+}
