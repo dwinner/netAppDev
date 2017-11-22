cd51c999d79e35ddb02243cc50d75cea9e66256f
bboytronik@gmail.com <bboytronik@gmail.com>
2017-11-22 18:59:44 +0300

    Added lang based adapter

---
+++ Object Oriented Pro/Structural Patterns/LanguageBasedAdapter/CustomCollection.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace LanguageBasedAdapter
+{
+   public class CustomCollection : IEnumerable<int>
+   {
+      private readonly List<int> _list = new List<int>();
+
+      public IEnumerator<int> GetEnumerator() => _list.GetEnumerator();
+
+      IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+      public void Insert(int value) => _list.Add(value);
+   }
+}
---
+++ Object Oriented Pro/Structural Patterns/LanguageBasedAdapter/CustomCollectionExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace LanguageBasedAdapter
+{
+   public static class CustomCollectionExtensions
+   {
+      public static void Add(this CustomCollection @this,
+         int value) => @this.Insert(value);
+   }
+}
