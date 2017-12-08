6e9924d6c386c2ac7ab4623693dbad6c69502f3b
DeVi <dwinner@inbox.ru>
2017-12-08 18:26:28 +0300

    Added dynamic visitor pattern

---
+++ Object Oriented Pro/Behavioral Patterns/VisitorViaDLR/PersonVisitor.cs
@@ -0,0 +1,13 @@
+﻿namespace VisitorViaDLR
+{
+   public abstract class PersonVisitor<T>
+   {
+      public T DynamicVisit(Person aPerson) => Visit((dynamic) aPerson);
+
+      protected abstract T Visit(Person aPerson);
+
+      protected virtual T Visit(Customer aCustomer) => Visit((Person) aCustomer);
+
+      protected virtual T Visit(Employee anEmployee) => Visit((Person) anEmployee);
+   }
+}
