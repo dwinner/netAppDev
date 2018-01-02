8976d43da1fb65736e0fed9aad6932c85e9d309d
jdeveloper85 <dwinner@inbox.ru>
2018-01-02 18:01:03 +0300

    Added dependency injection examples for .NET Core

---
+++ IoC Features/DotNetCore/DepLib/HelloController.cs
@@ -0,0 +1,11 @@
+﻿namespace DepLib
+{
+   public class HelloController
+   {
+      private readonly IGreetingService _greetingService;
+
+      public HelloController(IGreetingService greetingService) => _greetingService = greetingService;
+
+      public string Action(string name) => _greetingService.Greeting(name);
+   }
+}
