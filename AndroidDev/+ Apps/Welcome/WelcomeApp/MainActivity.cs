23c147ef4c623af4b84da7a1dc4fd7228abfaddb
bboytronik@gmail.com <bboytronik@gmail.com>
2018-01-12 22:27:51 +0300

    Android AppDev / Welcome app - Init

---
+++ AndroidDev/+ Apps/Welcome/WelcomeApp/MainActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.OS;
+using AppDevUnited.WelcomeApp;
+
+namespace WelcomeApp
+{
+   [Activity(Label = "WelcomeApp", MainLauncher = true)]
+   public class MainActivity : Activity
+   {
+      protected override void OnCreate(Bundle savedInstanceState)
+      {
+         base.OnCreate(savedInstanceState);         
+         SetContentView(Resource.Layout.Main);
+      }
+   }
+}
