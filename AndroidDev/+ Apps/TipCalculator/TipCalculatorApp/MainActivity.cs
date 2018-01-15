dde20b9e1d23545b3fd27402b98d01b5f4a3e369
bboytronik@gmail.com <bboytronik@gmail.com>
2018-01-15 21:10:45 +0300

    Android Dev / Tip calculator app / init

---
+++ AndroidDev/+ Apps/TipCalculator/TipCalculatorApp/MainActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace TipCalculatorApp
+{
+   [Activity(Label = "TipCalculatorApp", MainLauncher = true)]
+   public class MainActivity : AppCompatActivity
+   {
+      protected override void OnCreate(Bundle savedInstanceState)
+      {
+         base.OnCreate(savedInstanceState);
+
+         // Set our view from the "main" layout resource
+         SetContentView(Resource.Layout.Main);
+      }
+   }
+}
