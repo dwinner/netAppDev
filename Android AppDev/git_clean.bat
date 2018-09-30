c05a4daef2c6bc7160c6863e9afa4ebfef53828b
jdeveloper85 <dwinner@inbox.ru>
2018-09-30 13:49:01 +0300

    Point of intereset / files to code + back end

--- Android AppDev/git_clean.bat
+++ Android AppDev/git_clean.bat
@@ -5,6 +5,8 @@ for /d /r "." %%d in (.nuget) do @if exist "%%d" rd /s/q "%%d"
 for /d /r "." %%d in (Debug) do @if exist "%%d" rd /s/q "%%d"
 for /d /r "." %%d in (Release) do @if exist "%%d" rd /s/q "%%d"
 for /d /r "." %%d in (TestResults) do @if exist "%%d" rd /s/q "%%d"
+for /d /r "." %%d in (__MACOSX) do @if exist "%%d" rd /s/q "%%d"
 del /f /s /q .\*.sdf
 del /f /s /q .\*.user
+del /f /s /q .\*.DS_Store
 pause
