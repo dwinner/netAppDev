d0d2743466d0e3fc8e5da1f1eb422944fd564f52
DeVi <dwinner@inbox.ru>
2018-11-08 17:53:42 +0300

    Canon Game App / Init

---
+++ Android AppDev/+ Apps/CannonGame/CannonGame.App/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace CannonGame.App
+{
+   [Activity(
+      Label = "@string/app_name",
+      Theme = "@style/AppTheme.NoActionBar",
+      MainLauncher = true,
+      ScreenOrientation = ScreenOrientation.Landscape)]
+   public class MainActivity : AppCompatActivity
+   {
+      protected override void OnCreate(Bundle savedInstanceState)
+      {
+         base.OnCreate(savedInstanceState);
+         SetContentView(Resource.Layout.ActivityMain);
+      }
+   }
+}
