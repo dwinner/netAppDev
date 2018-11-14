1571fc1d53df469dc1f5218a6799c0c4dc530f14
DeVi <dwinner@inbox.ru>
2018-11-14 13:20:39 +0300

    POI App / misc

--- Android AppDev/+ Apps/PointOfView/PointOfViewApp/Resources/Resource.Designer.cs
+++ Android AppDev/+ Apps/PointOfView/PointOfViewApp/Resources/Resource.Designer.cs
@@ -3173,25 +3173,28 @@ namespace PointOfViewApp
 			public const int PoiDetail = 2130968631;
 			
 			// aapt resource value: 0x7f040038
-			public const int PoiList = 2130968632;
+			public const int PoiDetailFragment = 2130968632;
 			
 			// aapt resource value: 0x7f040039
-			public const int PoiListItem = 2130968633;
+			public const int PoiList = 2130968633;
 			
 			// aapt resource value: 0x7f04003a
-			public const int select_dialog_item_material = 2130968634;
+			public const int PoiListItem = 2130968634;
 			
 			// aapt resource value: 0x7f04003b
-			public const int select_dialog_multichoice_material = 2130968635;
+			public const int select_dialog_item_material = 2130968635;
 			
 			// aapt resource value: 0x7f04003c
-			public const int select_dialog_singlechoice_material = 2130968636;
+			public const int select_dialog_multichoice_material = 2130968636;
 			
 			// aapt resource value: 0x7f04003d
-			public const int support_simple_spinner_dropdown_item = 2130968637;
+			public const int select_dialog_singlechoice_material = 2130968637;
 			
 			// aapt resource value: 0x7f04003e
-			public const int tooltip = 2130968638;
+			public const int support_simple_spinner_dropdown_item = 2130968638;
+			
+			// aapt resource value: 0x7f04003f
+			public const int tooltip = 2130968639;
 			
 			static Layout()
 			{
