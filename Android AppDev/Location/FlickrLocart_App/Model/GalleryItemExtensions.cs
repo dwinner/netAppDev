a940be8f87c7d7e76ff29b6a1ab7af2430b74152
DeVi <dwinner@inbox.ru>
2018-11-20 16:28:17 +0300

    Location / Locator for Flickr / WIP

---
+++ Android AppDev/Location/FlickrLocart_App/Model/GalleryItemExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Android.Net;
+
+namespace FlickrLocart_App.Model
+{
+   public static class GalleryItemExtensions
+   {
+      private const string FlickrPhotosEndPoint = "http://www.flickr.com/photos/";
+
+      public static Uri GetPhotoPageUri(this GalleryItem galleryItem) =>
+         Uri.Parse(FlickrPhotosEndPoint)
+            .BuildUpon()
+            .AppendPath(galleryItem.Owner)
+            .AppendPath(galleryItem.Id)
+            .Build();
+   }
+}
