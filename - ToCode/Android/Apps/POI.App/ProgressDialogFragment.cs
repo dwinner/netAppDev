b9e1c0577317df1a14f8c0e79a8a53d47544a280
DeVi <dwinner@inbox.ru>
2018-11-20 10:49:31 +0300

    Android / misc

---
+++ - ToCode/Android/Apps/POI.App/ProgressDialogFragment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Android.App;
+
+namespace POIApp
+{
+	public class ProgressDialogFragment : DialogFragment
+	{
+		public override Dialog OnCreateDialog (Android.OS.Bundle savedInstanceState)
+		{
+			Cancelable = false;
+			ProgressDialog _progressDialog = new ProgressDialog (Activity);
+			_progressDialog.SetMessage ("Getting location...");
+			_progressDialog.Indeterminate = true;
+			_progressDialog.SetProgressStyle (ProgressDialogStyle.Spinner);
+			return _progressDialog;
+		}
+	}
+}
+
