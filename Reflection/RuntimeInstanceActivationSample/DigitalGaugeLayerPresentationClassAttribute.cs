ceb14b3bc6495609c4e08360f85ce667cc82d0e2
SCHLEISSHEIMER\Vinevtsev <dwinner@inbox.ru>
2018-12-13 14:28:23 +0300

    Added example about Runtime type creation

---
+++ Reflection/RuntimeInstanceActivationSample/DigitalGaugeLayerPresentationClassAttribute.cs
@@ -0,0 +1,31 @@
+﻿/**
+ * Runtime activation
+ */
+
+using System;
+using DevExpress.Xpf.Gauges;
+
+namespace RuntimeInstanceActivationSample
+{
+   [AttributeUsage(AttributeTargets.Field)]
+   public sealed class DigitalGaugeLayerPresentationClassAttribute : Attribute
+   {
+      private static readonly Func<Type, Type, bool> _CreationPredicate = (baseType, instanceType) =>
+         instanceType.IsClass && instanceType.IsPublic && !instanceType.IsAbstract
+         && baseType.IsAssignableFrom(instanceType);
+
+      public DigitalGaugeLayerPresentationClassAttribute(Type instanceType)
+      {
+         if (!_CreationPredicate(typeof(DigitalGaugeLayerPresentation), instanceType))
+         {
+            throw new ArgumentException(
+               $"{instanceType.Name} isn't inherited from {nameof(DigitalGaugeLayerPresentation)}",
+               nameof(instanceType));
+         }
+
+         InstanceType = instanceType;
+      }
+
+      public Type InstanceType { get; }
+   }
+}
---
+++ Reflection/RuntimeInstanceActivationSample/DigitalGaugeLayerPresentationKindExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace RuntimeInstanceActivationSample
+{
+   public static class DigitalGaugeLayerPresentationKindExtensions
+   {
+      public static Type GetDigitalGaugeLayerPresentationType(
+         this DigitalGaugeLayerPresentationKind digitalGaugeLayerPresentationKind) =>
+         digitalGaugeLayerPresentationKind
+            .GetType()
+            .GetField(digitalGaugeLayerPresentationKind.ToString())
+            .GetCustomAttributes(typeof(DigitalGaugeLayerPresentationClassAttribute), false)
+            .Cast<DigitalGaugeLayerPresentationClassAttribute>()
+            .FirstOrDefault()?.InstanceType;
+   }
+}
