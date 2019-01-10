220618f67f71d20a944b7fafb2e10a421b529441
SCHLEISSHEIMER\Vinevtsev <dwinner@inbox.ru>
2019-01-10 12:46:55 +0300

    Twitter search App / WIP

--- Android AppDev/+ Apps/TwitterSearches/TwitterSearches.App/MainActivity.cs
+++ Android AppDev/+ Apps/TwitterSearches/TwitterSearches.App/MainActivity.cs
@@ -1,14 +1,18 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
 using Android.OS;
 using Android.Support.Design.Widget;
 using Android.Support.V7.App;
+using Android.Support.V7.Widget;
 using Android.Views;
-using System;
-using AppDevUnited.TwitterSearches.App;
 
-namespace TwitterSearches.App
+namespace AppDevUnited.TwitterSearches.App
 {
-   [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+   [Activity(
+      Label = "@string/app_name",
+      Theme = "@style/AppTheme.NoActionBar",
+      MainLauncher = true,
+      WindowSoftInputMode = SoftInput.StateAlwaysHidden)]
    public class MainActivity : AppCompatActivity
    {
       protected override void OnCreate(Bundle savedInstanceState)
@@ -16,36 +20,15 @@ namespace TwitterSearches.App
          base.OnCreate(savedInstanceState);
          SetContentView(Resource.Layout.activity_main);
 
-         Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+         var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
          SetSupportActionBar(toolbar);
 
-         FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-         fab.Click += FabOnClick;
+         var fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+         fab.Click += OnFabClick;
       }
 
-      /*public override bool OnCreateOptionsMenu(IMenu menu)
+      private void OnFabClick(object sender, EventArgs eventArgs)
       {
-         MenuInflater.Inflate(Resource.Menu.menu_main, menu);
-         return true;
-      }
-
-      public override bool OnOptionsItemSelected(IMenuItem item)
-      {
-         int id = item.ItemId;
-         if (id == Resource.Id.action_settings)
-         {
-            return true;
-         }
-
-         return base.OnOptionsItemSelected(item);
-      }*/
-
-      private void FabOnClick(object sender, EventArgs eventArgs)
-      {
-         View view = (View)sender;
-         Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-             .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
       }
    }
 }
-
