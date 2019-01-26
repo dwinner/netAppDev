76ce5c692df3e59a65d79900083ebeff6bd8f9a4
bboytronik@gmail.com <bboytronik@gmail.com>
2019-01-26 12:27:32 +0300

    Address book App / layout dev - completed

--- Android AppDev/+ Apps/AddressBook/AddressBook.App/Resources/Resource.designer.cs
+++ Android AppDev/+ Apps/AddressBook/AddressBook.App/Resources/Resource.designer.cs
@@ -3095,8 +3095,8 @@ namespace AppDevUnited.AddressBook.App
 			// aapt resource value: 0x7f09003e
 			public const int SYM = 2131296318;
 			
-			// aapt resource value: 0x7f0900d0
-			public const int action0 = 2131296464;
+			// aapt resource value: 0x7f0900d1
+			public const int action0 = 2131296465;
 			
 			// aapt resource value: 0x7f09008d
 			public const int action_bar = 2131296397;
@@ -3119,23 +3119,23 @@ namespace AppDevUnited.AddressBook.App
 			// aapt resource value: 0x7f090069
 			public const int action_bar_title = 2131296361;
 			
-			// aapt resource value: 0x7f0900cd
-			public const int action_container = 2131296461;
+			// aapt resource value: 0x7f0900ce
+			public const int action_container = 2131296462;
 			
 			// aapt resource value: 0x7f09008e
 			public const int action_context_bar = 2131296398;
 			
+			// aapt resource value: 0x7f0900e4
+			public const int action_delete = 2131296484;
+			
+			// aapt resource value: 0x7f0900d5
+			public const int action_divider = 2131296469;
+			
 			// aapt resource value: 0x7f0900e3
-			public const int action_delete = 2131296483;
+			public const int action_edit = 2131296483;
 			
-			// aapt resource value: 0x7f0900d4
-			public const int action_divider = 2131296468;
-			
-			// aapt resource value: 0x7f0900e2
-			public const int action_edit = 2131296482;
-			
-			// aapt resource value: 0x7f0900ce
-			public const int action_image = 2131296462;
+			// aapt resource value: 0x7f0900cf
+			public const int action_image = 2131296463;
 			
 			// aapt resource value: 0x7f090003
 			public const int action_menu_divider = 2131296259;
@@ -3152,11 +3152,11 @@ namespace AppDevUnited.AddressBook.App
 			// aapt resource value: 0x7f09006b
 			public const int action_mode_close_button = 2131296363;
 			
-			// aapt resource value: 0x7f0900cf
-			public const int action_text = 2131296463;
+			// aapt resource value: 0x7f0900d0
+			public const int action_text = 2131296464;
 			
-			// aapt resource value: 0x7f0900dd
-			public const int actions = 2131296477;
+			// aapt resource value: 0x7f0900de
+			public const int actions = 2131296478;
 			
 			// aapt resource value: 0x7f09006c
 			public const int activity_chooser_view_content = 2131296364;
@@ -3164,8 +3164,8 @@ namespace AppDevUnited.AddressBook.App
 			// aapt resource value: 0x7f09002e
 			public const int add = 2131296302;
 			
-			// aapt resource value: 0x7f0900bd
-			public const int addButton = 2131296445;
+			// aapt resource value: 0x7f0900be
+			public const int addButton = 2131296446;
 			
 			// aapt resource value: 0x7f09007f
 			public const int alertTitle = 2131296383;
@@ -3194,8 +3194,8 @@ namespace AppDevUnited.AddressBook.App
 			// aapt resource value: 0x7f090072
 			public const int buttonPanel = 2131296370;
 			
-			// aapt resource value: 0x7f0900d1
-			public const int cancel_action = 2131296465;
+			// aapt resource value: 0x7f0900d2
+			public const int cancel_action = 2131296466;
 			
 			// aapt resource value: 0x7f09004c
 			public const int center = 2131296332;
@@ -3209,17 +3209,20 @@ namespace AppDevUnited.AddressBook.App
 			// aapt resource value: 0x7f090086
 			public const int checkbox = 2131296390;
 			
-			// aapt resource value: 0x7f0900d9
-			public const int chronometer = 2131296473;
+			// aapt resource value: 0x7f0900da
+			public const int chronometer = 2131296474;
 			
-			// aapt resource value: 0x7f0900c7
-			public const int cityLabelTextView = 2131296455;
-			
-			// aapt resource value: 0x7f0900b5
-			public const int cityTextInputLayout = 2131296437;
+			// aapt resource value: 0x7f0900b7
+			public const int cityEditText = 2131296439;
 			
 			// aapt resource value: 0x7f0900c8
-			public const int cityTextView = 2131296456;
+			public const int cityLabelTextView = 2131296456;
+			
+			// aapt resource value: 0x7f0900b6
+			public const int cityTextInputLayout = 2131296438;
+			
+			// aapt resource value: 0x7f0900c9
+			public const int cityTextView = 2131296457;
 			
 			// aapt resource value: 0x7f090061
 			public const int clip_horizontal = 2131296353;
@@ -3230,11 +3233,11 @@ namespace AppDevUnited.AddressBook.App
 			// aapt resource value: 0x7f090040
 			public const int collapseActionView = 2131296320;
 			
-			// aapt resource value: 0x7f09009f
-			public const int contactsFragment = 2131296415;
+			// aapt resource value: 0x7f0900a0
+			public const int contactsFragment = 2131296416;
 			
-			// aapt resource value: 0x7f0900a3
-			public const int container = 2131296419;
+			// aapt resource value: 0x7f0900a4
+			public const int container = 2131296420;
 			
 			// aapt resource value: 0x7f090082
 			public const int content = 2131296386;
@@ -3242,8 +3245,8 @@ namespace AppDevUnited.AddressBook.App
 			// aapt resource value: 0x7f090075
 			public const int contentPanel = 2131296373;
 			
-			// aapt resource value: 0x7f0900a4
-			public const int coordinator = 2131296420;
+			// aapt resource value: 0x7f0900a5
+			public const int coordinator = 2131296421;
 			
 			// aapt resource value: 0x7f09009d
 			public const int coordinatorLayout = 2131296413;
@@ -3260,65 +3263,47 @@ namespace AppDevUnited.AddressBook.App
 			// aapt resource value: 0x7f09006f
 			public const int default_activity_button = 2131296367;
 			
-			// aapt resource value: 0x7f0900a6
-			public const int design_bottom_sheet = 2131296422;
+			// aapt resource value: 0x7f0900a7
+			public const int design_bottom_sheet = 2131296423;
+			
+			// aapt resource value: 0x7f0900ac
+			public const int design_menu_item_action_area = 2131296428;
 			
 			// aapt resource value: 0x7f0900ab
-			public const int design_menu_item_action_area = 2131296427;
+			public const int design_menu_item_action_area_stub = 2131296427;
 			
 			// aapt resource value: 0x7f0900aa
-			public const int design_menu_item_action_area_stub = 2131296426;
+			public const int design_menu_item_text = 2131296426;
 			
 			// aapt resource value: 0x7f0900a9
-			public const int design_menu_item_text = 2131296425;
+			public const int design_navigation_view = 2131296425;
 			
-			// aapt resource value: 0x7f0900a8
-			public const int design_navigation_view = 2131296424;
-			
-			// aapt resource value: 0x7f0900be
-			public const int detailsScrollView = 2131296446;
+			// aapt resource value: 0x7f0900bf
+			public const int detailsScrollView = 2131296447;
 			
 			// aapt resource value: 0x7f090027
 			public const int disableHome = 2131296295;
 			
-			// aapt resource value: 0x7f0900ae
-			public const int editText = 2131296430;
-			
-			// aapt resource value: 0x7f0900b0
-			public const int editText2 = 2131296432;
-			
-			// aapt resource value: 0x7f0900b2
-			public const int editText3 = 2131296434;
-			
-			// aapt resource value: 0x7f0900b4
-			public const int editText4 = 2131296436;
-			
-			// aapt resource value: 0x7f0900b6
-			public const int editText5 = 2131296438;
-			
-			// aapt resource value: 0x7f0900b8
-			public const int editText6 = 2131296440;
-			
-			// aapt resource value: 0x7f0900ba
-			public const int editText7 = 2131296442;
-			
 			// aapt resource value: 0x7f09008f
 			public const int edit_query = 2131296399;
 			
-			// aapt resource value: 0x7f0900c3
-			public const int emailLabelTextView = 2131296451;
-			
-			// aapt resource value: 0x7f0900b1
-			public const int emailTextInputLayout = 2131296433;
+			// aapt resource value: 0x7f0900b3
+			public const int emailEditText = 2131296435;
 			
 			// aapt resource value: 0x7f0900c4
-			public const int emailTextView = 2131296452;
+			public const int emailLabelTextView = 2131296452;
+			
+			// aapt resource value: 0x7f0900b2
+			public const int emailTextInputLayout = 2131296434;
+			
+			// aapt resource value: 0x7f0900c5
+			public const int emailTextView = 2131296453;
 			
 			// aapt resource value: 0x7f090037
 			public const int end = 2131296311;
 			
-			// aapt resource value: 0x7f0900df
-			public const int end_padder = 2131296479;
+			// aapt resource value: 0x7f0900e0
+			public const int end_padder = 2131296480;
 			
 			// aapt resource value: 0x7f090046
 			public const int enterAlways = 2131296326;
@@ -3353,6 +3338,9 @@ namespace AppDevUnited.AddressBook.App
 			// aapt resource value: 0x7f090067
 			public const int forever = 2131296359;
 			
+			// aapt resource value: 0x7f09009f
+			public const int fragmentContainer = 2131296415;
+			
 			// aapt resource value: 0x7f09000a
 			public const int ghost_view = 2131296266;
 			
@@ -3368,8 +3356,8 @@ namespace AppDevUnited.AddressBook.App
 			// aapt resource value: 0x7f090071
 			public const int icon = 2131296369;
 			
-			// aapt resource value: 0x7f0900de
-			public const int icon_group = 2131296478;
+			// aapt resource value: 0x7f0900df
+			public const int icon_group = 2131296479;
 			
 			// aapt resource value: 0x7f090041
 			public const int ifRoom = 2131296321;
@@ -3377,8 +3365,8 @@ namespace AppDevUnited.AddressBook.App
 			// aapt resource value: 0x7f09006e
 			public const int image = 2131296366;
 			
-			// aapt resource value: 0x7f0900da
-			public const int info = 2131296474;
+			// aapt resource value: 0x7f0900db
+			public const int info = 2131296475;
 			
 			// aapt resource value: 0x7f090068
 			public const int italic = 2131296360;
@@ -3389,8 +3377,8 @@ namespace AppDevUnited.AddressBook.App
 			// aapt resource value: 0x7f09004e
 			public const int labeled = 2131296334;
 			
-			// aapt resource value: 0x7f0900a2
-			public const int largeLabel = 2131296418;
+			// aapt resource value: 0x7f0900a3
+			public const int largeLabel = 2131296419;
 			
 			// aapt resource value: 0x7f090054
 			public const int left = 2131296340;
@@ -3407,11 +3395,11 @@ namespace AppDevUnited.AddressBook.App
 			// aapt resource value: 0x7f090070
 			public const int list_item = 2131296368;
 			
-			// aapt resource value: 0x7f0900e1
-			public const int masked = 2131296481;
+			// aapt resource value: 0x7f0900e2
+			public const int masked = 2131296482;
 			
-			// aapt resource value: 0x7f0900d3
-			public const int media_actions = 2131296467;
+			// aapt resource value: 0x7f0900d4
+			public const int media_actions = 2131296468;
 			
 			// aapt resource value: 0x7f09009c
 			public const int message = 2131296412;
@@ -3431,17 +3419,20 @@ namespace AppDevUnited.AddressBook.App
 			// aapt resource value: 0x7f09002f
 			public const int multiply = 2131296303;
 			
-			// aapt resource value: 0x7f0900bf
-			public const int nameLabelTextView = 2131296447;
-			
-			// aapt resource value: 0x7f0900ad
-			public const int nameTextInputLayout = 2131296429;
+			// aapt resource value: 0x7f0900af
+			public const int nameEditText = 2131296431;
 			
 			// aapt resource value: 0x7f0900c0
-			public const int nameTextView = 2131296448;
+			public const int nameLabelTextView = 2131296448;
 			
-			// aapt resource value: 0x7f0900a7
-			public const int navigation_header_container = 2131296423;
+			// aapt resource value: 0x7f0900ae
+			public const int nameTextInputLayout = 2131296430;
+			
+			// aapt resource value: 0x7f0900c1
+			public const int nameTextView = 2131296449;
+			
+			// aapt resource value: 0x7f0900a8
+			public const int navigation_header_container = 2131296424;
 			
 			// aapt resource value: 0x7f090042
 			public const int never = 2131296322;
@@ -3452,14 +3443,14 @@ namespace AppDevUnited.AddressBook.App
 			// aapt resource value: 0x7f090025
 			public const int normal = 2131296293;
 			
-			// aapt resource value: 0x7f0900dc
-			public const int notification_background = 2131296476;
+			// aapt resource value: 0x7f0900dd
+			public const int notification_background = 2131296477;
+			
+			// aapt resource value: 0x7f0900d7
+			public const int notification_main_column = 2131296471;
 			
 			// aapt resource value: 0x7f0900d6
-			public const int notification_main_column = 2131296470;
-			
-			// aapt resource value: 0x7f0900d5
-			public const int notification_main_column_container = 2131296469;
+			public const int notification_main_column_container = 2131296470;
 			
 			// aapt resource value: 0x7f090060
 			public const int outline = 2131296352;
@@ -3473,14 +3464,17 @@ namespace AppDevUnited.AddressBook.App
 			// aapt resource value: 0x7f09000b
 			public const int parent_matrix = 2131296267;
 			
-			// aapt resource value: 0x7f0900c1
-			public const int phoneLabelTextView = 2131296449;
-			
-			// aapt resource value: 0x7f0900af
-			public const int phoneTextInputLayout = 2131296431;
+			// aapt resource value: 0x7f0900b1
+			public const int phoneEditText = 2131296433;
 			
 			// aapt resource value: 0x7f0900c2
-			public const int phoneTextView = 2131296450;
+			public const int phoneLabelTextView = 2131296450;
+			
+			// aapt resource value: 0x7f0900b0
+			public const int phoneTextInputLayout = 2131296432;
+			
+			// aapt resource value: 0x7f0900c3
+			public const int phoneTextView = 2131296451;
 			
 			// aapt resource value: 0x7f090058
 			public const int pin = 2131296344;
@@ -3494,23 +3488,23 @@ namespace AppDevUnited.AddressBook.App
 			// aapt resource value: 0x7f090087
 			public const int radio = 2131296391;
 			
-			// aapt resource value: 0x7f0900bc
-			public const int recyclerView = 2131296444;
+			// aapt resource value: 0x7f0900bd
+			public const int recyclerView = 2131296445;
 			
 			// aapt resource value: 0x7f090055
 			public const int right = 2131296341;
 			
-			// aapt resource value: 0x7f0900a0
-			public const int rightPaneContainer = 2131296416;
+			// aapt resource value: 0x7f0900a1
+			public const int rightPaneContainer = 2131296417;
 			
-			// aapt resource value: 0x7f0900db
-			public const int right_icon = 2131296475;
+			// aapt resource value: 0x7f0900dc
+			public const int right_icon = 2131296476;
 			
-			// aapt resource value: 0x7f0900d7
-			public const int right_side = 2131296471;
+			// aapt resource value: 0x7f0900d8
+			public const int right_side = 2131296472;
 			
-			// aapt resource value: 0x7f0900bb
-			public const int saveFloatingActionButton = 2131296443;
+			// aapt resource value: 0x7f0900bc
+			public const int saveFloatingActionButton = 2131296444;
 			
 			// aapt resource value: 0x7f09000c
 			public const int save_image_matrix = 2131296268;
@@ -3587,8 +3581,8 @@ namespace AppDevUnited.AddressBook.App
 			// aapt resource value: 0x7f09002c
 			public const int showTitle = 2131296300;
 			
-			// aapt resource value: 0x7f0900a1
-			public const int smallLabel = 2131296417;
+			// aapt resource value: 0x7f0900a2
+			public const int smallLabel = 2131296418;
 			
 			// aapt resource value: 0x7f090016
 			public const int snackbar_action = 2131296278;
@@ -3620,26 +3614,32 @@ namespace AppDevUnited.AddressBook.App
 			// aapt resource value: 0x7f090056
 			public const int start = 2131296342;
 			
-			// aapt resource value: 0x7f0900c9
-			public const int stateLabelTextView = 2131296457;
-			
-			// aapt resource value: 0x7f0900b7
-			public const int stateTextInputLayout = 2131296439;
+			// aapt resource value: 0x7f0900b9
+			public const int stateEditText = 2131296441;
 			
 			// aapt resource value: 0x7f0900ca
-			public const int stateTextView = 2131296458;
+			public const int stateLabelTextView = 2131296458;
 			
-			// aapt resource value: 0x7f0900d2
-			public const int status_bar_latest_event_content = 2131296466;
+			// aapt resource value: 0x7f0900b8
+			public const int stateTextInputLayout = 2131296440;
 			
-			// aapt resource value: 0x7f0900c5
-			public const int streetLabelTextView = 2131296453;
+			// aapt resource value: 0x7f0900cb
+			public const int stateTextView = 2131296459;
 			
-			// aapt resource value: 0x7f0900b3
-			public const int streetTextInputLayout = 2131296435;
+			// aapt resource value: 0x7f0900d3
+			public const int status_bar_latest_event_content = 2131296467;
+			
+			// aapt resource value: 0x7f0900b5
+			public const int streetEditText = 2131296437;
 			
 			// aapt resource value: 0x7f0900c6
-			public const int streetTextView = 2131296454;
+			public const int streetLabelTextView = 2131296454;
+			
+			// aapt resource value: 0x7f0900b4
+			public const int streetTextInputLayout = 2131296436;
+			
+			// aapt resource value: 0x7f0900c7
+			public const int streetTextView = 2131296455;
 			
 			// aapt resource value: 0x7f09005b
 			public const int stretch = 2131296347;
@@ -3677,8 +3677,8 @@ namespace AppDevUnited.AddressBook.App
 			// aapt resource value: 0x7f09005a
 			public const int textStart = 2131296346;
 			
-			// aapt resource value: 0x7f0900ac
-			public const int text_input_password_toggle = 2131296428;
+			// aapt resource value: 0x7f0900ad
+			public const int text_input_password_toggle = 2131296429;
 			
 			// aapt resource value: 0x7f090018
 			public const int textinput_counter = 2131296280;
@@ -3689,8 +3689,8 @@ namespace AppDevUnited.AddressBook.App
 			// aapt resource value: 0x7f09001a
 			public const int textinput_helper_text = 2131296282;
 			
-			// aapt resource value: 0x7f0900d8
-			public const int time = 2131296472;
+			// aapt resource value: 0x7f0900d9
+			public const int time = 2131296473;
 			
 			// aapt resource value: 0x7f090023
 			public const int title = 2131296291;
@@ -3710,8 +3710,8 @@ namespace AppDevUnited.AddressBook.App
 			// aapt resource value: 0x7f09007d
 			public const int topPanel = 2131296381;
 			
-			// aapt resource value: 0x7f0900a5
-			public const int touch_outside = 2131296421;
+			// aapt resource value: 0x7f0900a6
+			public const int touch_outside = 2131296422;
 			
 			// aapt resource value: 0x7f09000f
 			public const int transition_current_scene = 2131296271;
@@ -3743,8 +3743,8 @@ namespace AppDevUnited.AddressBook.App
 			// aapt resource value: 0x7f09001b
 			public const int view_offset_helper = 2131296283;
 			
-			// aapt resource value: 0x7f0900e0
-			public const int visible = 2131296480;
+			// aapt resource value: 0x7f0900e1
+			public const int visible = 2131296481;
 			
 			// aapt resource value: 0x7f090043
 			public const int withText = 2131296323;
@@ -3752,14 +3752,17 @@ namespace AppDevUnited.AddressBook.App
 			// aapt resource value: 0x7f090035
 			public const int wrap_content = 2131296309;
 			
-			// aapt resource value: 0x7f0900cb
-			public const int zipLabelTextView = 2131296459;
-			
-			// aapt resource value: 0x7f0900b9
-			public const int zipTextInputLayout = 2131296441;
+			// aapt resource value: 0x7f0900bb
+			public const int zipEditText = 2131296443;
 			
 			// aapt resource value: 0x7f0900cc
-			public const int zipTextView = 2131296460;
+			public const int zipLabelTextView = 2131296460;
+			
+			// aapt resource value: 0x7f0900ba
+			public const int zipTextInputLayout = 2131296442;
+			
+			// aapt resource value: 0x7f0900cd
+			public const int zipTextView = 2131296461;
 			
 			static Id()
 			{
