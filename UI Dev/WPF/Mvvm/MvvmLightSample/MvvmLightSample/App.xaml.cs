cff6ed0607b66ba76c816734257ef8de0383ba6c
SCHLEISSHEIMER\Vinevtsev <dwinner@inbox.ru>
2019-04-02 14:07:53 +0300

    MVVM light code snippets for WPF

---
+++ UI Dev/WPF/Mvvm/MvvmLightSample/MvvmLightSample/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using GalaSoft.MvvmLight.Threading;
+
+namespace MvvmLightSample
+{
+   public partial class App
+   {
+      static App()
+      {
+         DispatcherHelper.Initialize();
+      }
+   }
+}
---
+++ UI Dev/WPF/Mvvm/MvvmLightSample/MvvmLightSample/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using MvvmLightSample.ViewModel;
+
+namespace MvvmLightSample
+{
+   public partial class MainWindow
+   {
+      /// <summary>
+      ///    Initializes a new instance of the MainWindow class.
+      /// </summary>
+      public MainWindow()
+      {
+         InitializeComponent();
+         Closing += (s, e) => ViewModelLocator.Cleanup();
+      }
+   }
+}
