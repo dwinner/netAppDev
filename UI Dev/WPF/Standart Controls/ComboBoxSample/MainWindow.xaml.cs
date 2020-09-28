98d8430812917102156c515750a3bbd67100c46c
SCHLEISSHEIMER\Vinevtsev <dwinner@inbox.ru>
2020-09-28 11:58:31 +0300

    Updated combo box items

--- UI Dev/WPF/Standart Controls/ComboBoxSample/MainWindow.xaml.cs
+++ UI Dev/WPF/Standart Controls/ComboBoxSample/MainWindow.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace ComboBoxSample
 {
    public partial class MainWindow
    {
+      private bool _handled = true;
+
       public MainWindow()
       {
          InitializeComponent();
@@ -12,7 +15,25 @@ namespace ComboBoxSample
 
       private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
       {
-         var comboBox = (ComboBox) sender;
+         if (sender is ComboBox box)
+         {
+            _handled = !box.IsDropDownOpen;
+            Handle();
+         }
+      }
+
+      private void OnDropDownClosed(object sender, EventArgs e)
+      {
+         if (_handled)
+         {
+            Handle();
+         }
+
+         _handled = true;
+      }
+
+      private void Handle()
+      {
          var selectedItem = (ComboBoxItem) comboBox.SelectedItem;
          if (selectedItem.Content != null)
          {
