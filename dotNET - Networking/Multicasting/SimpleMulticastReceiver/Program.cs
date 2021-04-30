97e6a214e346d5dcaf75acd3508e64f8421a935a
SCHLEISSHEIMER\Vinevtsev <dwinner@inbox.ru>
2021-04-30 10:22:56 +0300

    Multicasting wip

---
+++ dotNET - Networking/Multicasting/SimpleMulticastReceiver/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SimpleMulticastReceiver
+{
+   internal static class Program
+   {
+      private static void Main(string[] args)
+      {
+         var receiver = new MulticastReceiver();
+         receiver.Receive("224.5.6.7", "5000");
+      }
+   }
+}
