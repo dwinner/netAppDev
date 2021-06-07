a79c1f97191d3c782d4a64edbef0d653cb57ff07
vinevtsev <vinevtsev@schleissheimer.de>
2021-06-07 17:00:01 +0200

    Cleaning up some code

--- Data - EF Core/EF Core - samples/01-Intro/Book.cs
+++ Data - EF Core/EF Core - samples/01-Intro/Book.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace _01_Intro
 {
-   [Table("Books")]
+   [Table(nameof(BooksContext.Books))]
    public class Book
    {
       [Key]
--- Data - EF Core/EF Core - samples/01-Intro/Program.cs
+++ Data - EF Core/EF Core - samples/01-Intro/Program.cs
@@ -14,24 +14,22 @@ namespace _01_Intro
       private static async Task Main()
       {
          var program = new Program();
-         await program.CreateTheDatabaseAsync();
-         await program.AddBookAsync("Professional C# 7", "Wrox Press");
-         await program.AddBooksAsync();
-         await program.ReadBooksAsync();
-         await program.QueryBooksAsync();
-         await program.UpdateBookAsync();
-         await program.DeleteBooksAsync();
-         await program.DeleteDatabaseAsync();
+         await program.CreateTheDatabaseAsync().ConfigureAwait(false);
+         await program.AddBookAsync("Professional C# 7", "Wrox Press").ConfigureAwait(false);
+         await program.AddBooksAsync().ConfigureAwait(false);
+         await program.ReadBooksAsync().ConfigureAwait(false);
+         await program.QueryBooksAsync().ConfigureAwait(false);
+         await program.UpdateBookAsync().ConfigureAwait(false);
+         await program.DeleteBooksAsync().ConfigureAwait(false);
+         await program.DeleteDatabaseAsync().ConfigureAwait(false);
       }
 
       private async Task CreateTheDatabaseAsync()
       {
-         using (var context = new BooksContext())
-         {
-            var created = await context.Database.EnsureCreatedAsync();
-            var creationInfo = created ? "created" : "exists";
-            Console.WriteLine($"database {creationInfo}");
-         }
+         await using var context = new BooksContext();
+         var created = await context.Database.EnsureCreatedAsync().ConfigureAwait(false);
+         var creationInfo = created ? "created" : "exists";
+         Console.WriteLine($"database {creationInfo}");
       }
 
       private async Task DeleteDatabaseAsync()
@@ -40,22 +38,20 @@ namespace _01_Intro
          var input = Console.ReadLine();
          if (input.ToLower() == "y")
          {
-            using (var context = new BooksContext())
-            {
-               var deleted = await context.Database.EnsureDeletedAsync();
-               var deletionInfo = deleted ? "deleted" : "not deleted";
-               Console.WriteLine($"database {deletionInfo}");
-            }
+            await using var context = new BooksContext();
+            var deleted = await context.Database.EnsureDeletedAsync().ConfigureAwait(false);
+            var deletionInfo = deleted ? "deleted" : "not deleted";
+            Console.WriteLine($"database {deletionInfo}");
          }
       }
 
       private async Task AddBookAsync(string title, string publisher)
       {
-         using (var context = new BooksContext())
+         await using (var context = new BooksContext())
          {
             var book = new Book {Title = title, Publisher = publisher};
-            await context.Books.AddAsync(book);
-            var records = await context.SaveChangesAsync();
+            await context.Books.AddAsync(book).ConfigureAwait(false);
+            var records = await context.SaveChangesAsync().ConfigureAwait(false);
 
             Console.WriteLine($"{records} record added");
          }
@@ -65,14 +61,14 @@ namespace _01_Intro
 
       private async Task AddBooksAsync()
       {
-         using (var context = new BooksContext())
+         await using (var context = new BooksContext())
          {
             var b1 = new Book {Title = "Professional C# 6 and .NET Core 1.0", Publisher = "Wrox Press"};
             var b2 = new Book {Title = "Professional C# 5 and .NET 4.5.1", Publisher = "Wrox Press"};
             var b3 = new Book {Title = "JavaScript for Kids", Publisher = "Wrox Press"};
             var b4 = new Book {Title = "Web Design with HTML and CSS", Publisher = "For Dummies"};
-            await context.Books.AddRangeAsync(b1, b2, b3, b4);
-            var records = await context.SaveChangesAsync();
+            await context.Books.AddRangeAsync(b1, b2, b3, b4).ConfigureAwait(false);
+            var records = await context.SaveChangesAsync().ConfigureAwait(false);
 
             Console.WriteLine($"{records} records added");
          }
@@ -82,9 +78,9 @@ namespace _01_Intro
 
       private async Task ReadBooksAsync()
       {
-         using (var context = new BooksContext())
+         await using (var context = new BooksContext())
          {
-            var books = await context.Books.ToListAsync();
+            var books = await context.Books.ToListAsync().ConfigureAwait(false);
             foreach (var b in books)
             {
                Console.WriteLine($"{b.Title} {b.Publisher}");
@@ -96,11 +92,11 @@ namespace _01_Intro
 
       private async Task QueryBooksAsync()
       {
-         using (var context = new BooksContext())
+         await using (var context = new BooksContext())
          {
             var wroxBooks = await context.Books
                .Where(b => b.Publisher == "Wrox Press")
-               .ToListAsync();
+               .ToListAsync().ConfigureAwait(false);
 
             // comment the previous lines and uncomment the next lines to try the LINQ query syntax
             //var wroxBooks = await (from b in context.Books
@@ -118,16 +114,16 @@ namespace _01_Intro
 
       private async Task UpdateBookAsync()
       {
-         using (var context = new BooksContext())
+         await using (var context = new BooksContext())
          {
             var records = 0;
             var book = await context.Books
                .Where(b => b.Title == "Professional C# 7")
-               .FirstOrDefaultAsync();
+               .FirstOrDefaultAsync().ConfigureAwait(false);
             if (book != null)
             {
                book.Title = "Professional C# 7 and .NET Core 2.0";
-               records = await context.SaveChangesAsync();
+               records = await context.SaveChangesAsync().ConfigureAwait(false);
             }
 
             Console.WriteLine($"{records} record updated");
@@ -138,11 +134,11 @@ namespace _01_Intro
 
       private async Task DeleteBooksAsync()
       {
-         using (var context = new BooksContext())
+         await using (var context = new BooksContext())
          {
-            var books = await context.Books.ToListAsync();
+            var books = await context.Books.ToListAsync().ConfigureAwait(false);
             context.Books.RemoveRange(books);
-            var records = await context.SaveChangesAsync();
+            var records = await context.SaveChangesAsync().ConfigureAwait(false);
             Console.WriteLine($"{records} records deleted");
          }
 
