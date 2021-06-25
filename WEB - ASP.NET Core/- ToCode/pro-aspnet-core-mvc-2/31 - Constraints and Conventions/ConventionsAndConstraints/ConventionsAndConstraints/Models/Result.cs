8843f050bcaba61b691da61963ea101cd1274a17
SCHLEISSHEIMER\Vinevtsev <dwinner@inbox.ru>
2021-06-25 14:33:48 +0300

    some updates

--- WEB - ASP.NET Core/- ToCode/pro-aspnet-core-mvc-2/31 - Constraints and Conventions/ConventionsAndConstraints/ConventionsAndConstraints/Models/Result.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace ConventionsAndConstraints.Models {
-
-    public class Result {
-        public string Controller { get; set; }
-        public string Action { get; set; }
-    }
-}
--- WEB - ASP.NET Core/- ToCode/pro-aspnet-core-mvc-2/contributing.md
+++
@@ -1,3 +0,0 @@
-# Contributing to This Repository
-
-Please email the author at adam@adam-freeman.com with any corrections.
