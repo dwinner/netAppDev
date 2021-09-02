938d0258a749d14e9fbdaa893eb8f2716702ad35
vinevtsev <vinevtsev@schleissheimer.de>
2021-09-02 10:23:39 +0200

    relocation

--- Metaprogramming/Antrl/Capl_grammar/CaplGrammar.Sandbox/Program.cs
+++ Metaprogramming/Antrl/Capl_grammar/CaplGrammar.Sandbox/Program.cs
@@ -6,7 +6,7 @@ namespace CaplGrammar.Sandbox
    {
       static void Main(string[] args)
       {
-         Console.WriteLine("Hello World!");
+         Console.WriteLine("CAPL grammar");
       }
    }
 }
