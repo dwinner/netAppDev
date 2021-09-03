5f19eb98309ed9926b21c23e04bede8e2a9f4480
vinevtsev <vinevtsev@schleissheimer.de>
2021-09-03 07:45:05 +0200

    Design CAPL grammar based on C

---
+++ Metaprogramming/Antrl/Capl_grammar/CaplGrammar_Sandbox/grammars/.antlr/ExampleParser.java
@@ -0,0 +1,252 @@
+// Generated from d:\Projects\dotNET\appDev-NET\Metaprogramming\Antrl\Capl_grammar\CaplGrammar.Sandbox\grammars\Example.g4 by ANTLR 4.8
+import org.antlr.v4.runtime.atn.*;
+import org.antlr.v4.runtime.dfa.DFA;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.*;
+import org.antlr.v4.runtime.tree.*;
+import java.util.List;
+import java.util.Iterator;
+import java.util.ArrayList;
+
+@SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
+public class ExampleParser extends Parser {
+	static { RuntimeMetaData.checkVersion("4.8", RuntimeMetaData.VERSION); }
+
+	protected static final DFA[] _decisionToDFA;
+	protected static final PredictionContextCache _sharedContextCache =
+		new PredictionContextCache();
+	public static final int
+		LETTER=1, DIGIT=2, WS=3;
+	public static final int
+		RULE_start = 0, RULE_letters = 1, RULE_digits = 2;
+	private static String[] makeRuleNames() {
+		return new String[] {
+			"start", "letters", "digits"
+		};
+	}
+	public static final String[] ruleNames = makeRuleNames();
+
+	private static String[] makeLiteralNames() {
+		return new String[] {
+		};
+	}
+	private static final String[] _LITERAL_NAMES = makeLiteralNames();
+	private static String[] makeSymbolicNames() {
+		return new String[] {
+			null, "LETTER", "DIGIT", "WS"
+		};
+	}
+	private static final String[] _SYMBOLIC_NAMES = makeSymbolicNames();
+	public static final Vocabulary VOCABULARY = new VocabularyImpl(_LITERAL_NAMES, _SYMBOLIC_NAMES);
+
+	/**
+	 * @deprecated Use {@link #VOCABULARY} instead.
+	 */
+	@Deprecated
+	public static final String[] tokenNames;
+	static {
+		tokenNames = new String[_SYMBOLIC_NAMES.length];
+		for (int i = 0; i < tokenNames.length; i++) {
+			tokenNames[i] = VOCABULARY.getLiteralName(i);
+			if (tokenNames[i] == null) {
+				tokenNames[i] = VOCABULARY.getSymbolicName(i);
+			}
+
+			if (tokenNames[i] == null) {
+				tokenNames[i] = "<INVALID>";
+			}
+		}
+	}
+
+	@Override
+	@Deprecated
+	public String[] getTokenNames() {
+		return tokenNames;
+	}
+
+	@Override
+
+	public Vocabulary getVocabulary() {
+		return VOCABULARY;
+	}
+
+	@Override
+	public String getGrammarFileName() { return "Example.g4"; }
+
+	@Override
+	public String[] getRuleNames() { return ruleNames; }
+
+	@Override
+	public String getSerializedATN() { return _serializedATN; }
+
+	@Override
+	public ATN getATN() { return _ATN; }
+
+	public ExampleParser(TokenStream input) {
+		super(input);
+		_interp = new ParserATNSimulator(this,_ATN,_decisionToDFA,_sharedContextCache);
+	}
+
+	public static class StartContext extends ParserRuleContext {
+		public List<DigitsContext> digits() {
+			return getRuleContexts(DigitsContext.class);
+		}
+		public DigitsContext digits(int i) {
+			return getRuleContext(DigitsContext.class,i);
+		}
+		public List<LettersContext> letters() {
+			return getRuleContexts(LettersContext.class);
+		}
+		public LettersContext letters(int i) {
+			return getRuleContext(LettersContext.class,i);
+		}
+		public TerminalNode EOF() { return getToken(ExampleParser.EOF, 0); }
+		public StartContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_start; }
+	}
+
+	public final StartContext start() throws RecognitionException {
+		StartContext _localctx = new StartContext(_ctx, getState());
+		enterRule(_localctx, 0, RULE_start);
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(6);
+			digits();
+			setState(7);
+			letters();
+			setState(8);
+			digits();
+			setState(9);
+			letters();
+			setState(10);
+			match(EOF);
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static class LettersContext extends ParserRuleContext {
+		public List<TerminalNode> LETTER() { return getTokens(ExampleParser.LETTER); }
+		public TerminalNode LETTER(int i) {
+			return getToken(ExampleParser.LETTER, i);
+		}
+		public LettersContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_letters; }
+	}
+
+	public final LettersContext letters() throws RecognitionException {
+		LettersContext _localctx = new LettersContext(_ctx, getState());
+		enterRule(_localctx, 2, RULE_letters);
+		try {
+			int _alt;
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(13); 
+			_errHandler.sync(this);
+			_alt = 1;
+			do {
+				switch (_alt) {
+				case 1:
+					{
+					{
+					setState(12);
+					match(LETTER);
+					}
+					}
+					break;
+				default:
+					throw new NoViableAltException(this);
+				}
+				setState(15); 
+				_errHandler.sync(this);
+				_alt = getInterpreter().adaptivePredict(_input,0,_ctx);
+			} while ( _alt!=2 && _alt!=org.antlr.v4.runtime.atn.ATN.INVALID_ALT_NUMBER );
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static class DigitsContext extends ParserRuleContext {
+		public List<TerminalNode> DIGIT() { return getTokens(ExampleParser.DIGIT); }
+		public TerminalNode DIGIT(int i) {
+			return getToken(ExampleParser.DIGIT, i);
+		}
+		public DigitsContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_digits; }
+	}
+
+	public final DigitsContext digits() throws RecognitionException {
+		DigitsContext _localctx = new DigitsContext(_ctx, getState());
+		enterRule(_localctx, 4, RULE_digits);
+		int _la;
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(20);
+			_errHandler.sync(this);
+			_la = _input.LA(1);
+			while (_la==DIGIT) {
+				{
+				{
+				setState(17);
+				match(DIGIT);
+				}
+				}
+				setState(22);
+				_errHandler.sync(this);
+				_la = _input.LA(1);
+			}
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static final String _serializedATN =
+		"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\5\32\4\2\t\2\4\3"+
+		"\t\3\4\4\t\4\3\2\3\2\3\2\3\2\3\2\3\2\3\3\6\3\20\n\3\r\3\16\3\21\3\4\7"+
+		"\4\25\n\4\f\4\16\4\30\13\4\3\4\2\2\5\2\4\6\2\2\2\30\2\b\3\2\2\2\4\17\3"+
+		"\2\2\2\6\26\3\2\2\2\b\t\5\6\4\2\t\n\5\4\3\2\n\13\5\6\4\2\13\f\5\4\3\2"+
+		"\f\r\7\2\2\3\r\3\3\2\2\2\16\20\7\3\2\2\17\16\3\2\2\2\20\21\3\2\2\2\21"+
+		"\17\3\2\2\2\21\22\3\2\2\2\22\5\3\2\2\2\23\25\7\4\2\2\24\23\3\2\2\2\25"+
+		"\30\3\2\2\2\26\24\3\2\2\2\26\27\3\2\2\2\27\7\3\2\2\2\30\26\3\2\2\2\4\21"+
+		"\26";
+	public static final ATN _ATN =
+		new ATNDeserializer().deserialize(_serializedATN.toCharArray());
+	static {
+		_decisionToDFA = new DFA[_ATN.getNumberOfDecisions()];
+		for (int i = 0; i < _ATN.getNumberOfDecisions(); i++) {
+			_decisionToDFA[i] = new DFA(_ATN.getDecisionState(i), i);
+		}
+	}
+}
