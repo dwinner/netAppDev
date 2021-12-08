b3d2af00ea5c21d313a8e597df498670ddfae5ce
vinevtsev <vinevtsev@schleissheimer.de>
2021-12-08 14:48:17 +0100

    Adding vscode install extensions

---
+++ UI/WPF/+ Apps/VsCodeInstallExtensions/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace VsCodeInstallExtensions
+{
+    internal static class Program
+    {
+        private static int Main(string[] args)
+        {
+            if (args?.Length != 1)
+            {
+                return 1;
+            }
+
+            var vsCodePath = args[0];
+            var installer = new VsCodeExtensionInstaller(vsCodePath);
+            installer.Install();
+
+            return 0;
+        }
+    }
+}
