ef774b807d4aabcdf04e9a80473c5de4f343f99d
vinevtsev <vinevtsev@schleissheimer.de>
2021-12-20 07:35:32 +0100

    Fix definition visitor

--- Metaprogramming/Antrl/Capl/CaplGrammar.App/Impl/SymbolUsageValidation/DefinitionPhaseVisitor.cs
+++ Metaprogramming/Antrl/Capl/CaplGrammar.App/Impl/SymbolUsageValidation/DefinitionPhaseVisitor.cs
@@ -10,181 +10,186 @@ using CaplGrammar.Core;
 
 namespace CaplGrammar.Application.Impl.SymbolUsageValidation
 {
-   internal sealed class DefinitionPhaseVisitor : CaplBaseListener
-   {
-      private IScope _currentScope; // define symbols in this scope
-      private bool _isVariableBlock;
+    internal sealed class DefinitionPhaseVisitor : CaplBaseListener
+    {
+        private IScope _currentScope; // define symbols in this scope
+        private bool _isVariableBlock;
 
-      // NOTE maybe you need to extend it for more flexible using
-      public ParseTreeProperty<IScope> Scopes { get; } = new ParseTreeProperty<IScope>();
+        // NOTE maybe you need to extend it for more flexible using
+        public ParseTreeProperty<IScope> Scopes { get; } = new ParseTreeProperty<IScope>();
 
-      public GlobalSpaceScope GlobalScope { get; private set; }
+        public GlobalSpaceScope GlobalScope { get; private set; }
 
-      public override void EnterPrimaryExpression(CaplParser.PrimaryExpressionContext context)
-      {
-         // primary expr is left recursive, so the root is being walking only once: when there is no parent context
-         var parentCtx = context.Parent;
-         if (parentCtx != null)
-         {
-            // dont't reenter if it's not the root
-            return;
-         }
+        public override void EnterPrimaryExpression(CaplParser.PrimaryExpressionContext context)
+        {
+            // primary expr is left recursive, so the root is being walking only once: when there is no parent context
+            var parentCtx = context.Parent;
+            if (parentCtx != null)
+            {
+                // dont't reenter if it's not the root
+                return;
+            }
 
-         GlobalScope = new GlobalSpaceScope(null);
-         _currentScope = GlobalScope;
-      }
+            GlobalScope = new GlobalSpaceScope(null);
+            _currentScope = GlobalScope;
+        }
 
-      public override void ExitPrimaryExpression(CaplParser.PrimaryExpressionContext context)
-      {
-         Debug.WriteLine((object)GlobalScope);
-      }
+        public override void ExitPrimaryExpression(CaplParser.PrimaryExpressionContext context)
+        {
+            Debug.WriteLine(GlobalScope);
+        }
 
-      public override void EnterVariableBlock(CaplParser.VariableBlockContext context)
-      {
-         _currentScope = GlobalScope;
-         _isVariableBlock = true;
-      }
+        public override void EnterVariableBlock(CaplParser.VariableBlockContext context)
+        {
+            _currentScope = GlobalScope;
+            _isVariableBlock = true;
+        }
 
-      public override void ExitVariableBlock(CaplParser.VariableBlockContext context)
-      {
-         Debug.WriteLine((object)_currentScope);
-         _currentScope = GlobalScope;
-         _isVariableBlock = false;
-      }
+        public override void ExitVariableBlock(CaplParser.VariableBlockContext context)
+        {
+            Debug.WriteLine(_currentScope);
+            _currentScope = GlobalScope;
+            _isVariableBlock = false;
+        }
 
-      public override void EnterFunctionDefinition(CaplParser.FunctionDefinitionContext context)
-      {
-         var funcName = context.declarator()?.directDeclarator()?.directDeclarator()?.Identifier()?.GetText()
-                        ?? throw new ArgumentNullException(nameof(EnterFunctionDefinition),
-                           "Unable to select function name");
+        public override void EnterFunctionDefinition(CaplParser.FunctionDefinitionContext context)
+        {
+            var funcName = context.declarator()?.directDeclarator()?.directDeclarator()?.Identifier()?.GetText()
+                           ?? throw new ArgumentNullException(nameof(EnterFunctionDefinition),
+                               "Unable to select function name");
 
-         var typeSpecifier = context.declarationSpecifiers()?.declarationSpecifier()
-            .Select(declSpec => declSpec.typeSpecifier())
-            .FirstOrDefault(ctx => ctx != null);
+            var typeSpecifier = context.declarationSpecifiers()?.declarationSpecifier()
+                .Select(declSpec => declSpec.typeSpecifier())
+                .FirstOrDefault(ctx => ctx != null);
 
-         var returnType = typeSpecifier?.Start.Type.GetCaplType() ?? BuiltInType.Void;
-         var userTypeToken = GetUserTypeToken(returnType, typeSpecifier);
-         var userDefType = userTypeToken?.Text ?? string.Empty;
+            var returnType = typeSpecifier?.Start.Type.GetCaplType() ?? BuiltInType.Void;
+            var userTypeToken = GetUserTypeToken(returnType, typeSpecifier);
+            var userDefType = userTypeToken?.Text ?? string.Empty;
 
-         // push new scope by making new one that points to enclosing scope
-         var function = new FunctionSymbol(funcName, returnType, _currentScope, userDefType);
-         _currentScope.Define(function); // define function in current scope
-         SaveScope(context, function); // push: set function's parent to current
-         _currentScope = function;
-      }
+            // push new scope by making new one that points to enclosing scope
+            var function = new FunctionSymbol(funcName, returnType, _currentScope, userDefType);
+            _currentScope.Define(function); // define function in current scope
+            SaveScope(context, function); // push: set function's parent to current
+            _currentScope = function;
+        }
 
-      private static IToken GetUserTypeToken(BuiltInType builtInType, CaplParser.TypeSpecifierContext typeSpecCtx)
-      {
-         switch (builtInType)
-         {
-            case BuiltInType.Struct:
-               return typeSpecCtx.structSpecifier()?.Identifier()?.Symbol;
-            case BuiltInType.Enum:
-               return typeSpecCtx.enumSpecifier()?.Identifier()?.Symbol;
-            default:
-               return null;
-         }
-      }
+        private static IToken GetUserTypeToken(BuiltInType builtInType, CaplParser.TypeSpecifierContext typeSpecCtx)
+        {
+            switch (builtInType)
+            {
+                case BuiltInType.Struct:
+                    return typeSpecCtx.structSpecifier()?.Identifier()?.Symbol;
+                case BuiltInType.Enum:
+                    return typeSpecCtx.enumSpecifier()?.Identifier()?.Symbol;
+                default:
+                    return null;
+            }
+        }
 
-      public override void ExitFunctionDefinition(CaplParser.FunctionDefinitionContext context)
-      {
-         Debug.WriteLine((object)_currentScope);
-         _currentScope = _currentScope?.EnclosingScope;
-      }
+        public override void ExitFunctionDefinition(CaplParser.FunctionDefinitionContext context)
+        {
+            Debug.WriteLine(_currentScope);
+            _currentScope = _currentScope?.EnclosingScope;
+        }
 
-      public override void EnterBlockItemList(CaplParser.BlockItemListContext context)
-      {
-         // Variables-block is like a global scope in CAPL
-         if (_isVariableBlock)
-         {
-            return;
-         }
+        public override void EnterBlockItemList(CaplParser.BlockItemListContext context)
+        {
+            // Variables-block is like a global scope in CAPL
+            if (_isVariableBlock)
+            {
+                return;
+            }
 
-         // push new local scope
-         var localScope = new LocalScope(_currentScope);
-         _currentScope = localScope;
-         SaveScope(context, _currentScope);
-      }
+            // push new local scope
+            var localScope = new LocalScope(_currentScope);
+            _currentScope = localScope;
+            SaveScope(context, _currentScope);
+        }
 
-      public override void ExitBlockItemList(CaplParser.BlockItemListContext context)
-      {
-         // Keep in mind that variables-scope has no nested scopes due to our handling anymore
-         if (_isVariableBlock)
-         {
-            return;
-         }
+        public override void ExitBlockItemList(CaplParser.BlockItemListContext context)
+        {
+            // Keep in mind that variables-scope has no nested scopes due to our handling anymore
+            if (_isVariableBlock)
+            {
+                return;
+            }
 
-         Debug.WriteLine((object)_currentScope);
-         _currentScope = _currentScope?.EnclosingScope; // pop scope
-      }
+            Debug.WriteLine(_currentScope);
+            _currentScope = _currentScope?.EnclosingScope; // pop scope
+        }
 
-      public override void ExitParameterDeclaration(CaplParser.ParameterDeclarationContext context)
-      {
-         var declSpecCtx = context.declarationSpecifiers().declarationSpecifier();
-         var typeSpecCtx = GetTypeSpecContext(declSpecCtx);
-         if (typeSpecCtx == null)
-         {
-            return;
-         }
+        public override void ExitParameterDeclaration(CaplParser.ParameterDeclarationContext context)
+        {
+            var declSpecCtx = context.declarationSpecifiers()?.declarationSpecifier();
+            if (declSpecCtx == null)
+            {
+                return;
+            }
 
-         var declType = typeSpecCtx.Start.Type.GetCaplType();
-         var userTypeToken = GetUserTypeToken(declType, typeSpecCtx);
+            var typeSpecCtx = GetTypeSpecContext(declSpecCtx);
+            if (typeSpecCtx == null)
+            {
+                return;
+            }
 
-         var directDecl = context.declarator()?.directDeclarator();
-         var declToken = GetTypeToken(directDecl);
+            var declType = typeSpecCtx.Start.Type.GetCaplType();
+            var userTypeToken = GetUserTypeToken(declType, typeSpecCtx);
 
-         DefineVar(declType, declToken, userTypeToken);
-      }
+            var directDecl = context.declarator()?.directDeclarator();
+            var declToken = GetTypeToken(directDecl);
 
-      public override void ExitDeclaration(CaplParser.DeclarationContext context)
-      {
-         // Get the type for variable declaration
-         var declSpecCtx = context.declarationSpecifiers().declarationSpecifier();
-         var typeSpecCtx = GetTypeSpecContext(declSpecCtx);
-         if (typeSpecCtx == null)
-         {
-            return;
-         }
+            DefineVar(declType, declToken, userTypeToken);
+        }
 
-         var declType = typeSpecCtx.Start.Type.GetCaplType();
-         var userTypeToken = GetUserTypeToken(declType, typeSpecCtx);
+        public override void ExitDeclaration(CaplParser.DeclarationContext context)
+        {
+            // Get the type for variable declaration
+            var declSpecCtx = context.declarationSpecifiers().declarationSpecifier();
+            var typeSpecCtx = GetTypeSpecContext(declSpecCtx);
+            if (typeSpecCtx == null)
+            {
+                return;
+            }
 
-         // Get variable(s) name(s)
-         var declTokenList = context.initDeclaratorList().initDeclarator()
-            .Select(ctx => GetTypeToken(ctx.declarator()?.directDeclarator()))
-            .ToList();
+            var declType = typeSpecCtx.Start.Type.GetCaplType();
+            var userTypeToken = GetUserTypeToken(declType, typeSpecCtx);
 
-         declTokenList.ForEach(idToken => DefineVar(declType, idToken, userTypeToken));
-      }
+            // Get variable(s) name(s)
+            var declTokenList = context.initDeclaratorList().initDeclarator()
+                .Select(ctx => GetTypeToken(ctx.declarator()?.directDeclarator()))
+                .ToList();
 
-      private void DefineVar(BuiltInType aType, IToken aNameToken, IToken userType)
-      {
-         var userDefType = userType?.Text ?? string.Empty;
-         var variable = new VariableSymbol(aNameToken.Text, aType, userDefType);
-         _currentScope.Define(variable);
-      }
+            declTokenList.ForEach(idToken => DefineVar(declType, idToken, userTypeToken));
+        }
 
-      private void SaveScope(IParseTree aContext, IScope aScope)
-      {
-         Scopes.Put(aContext, aScope);
-      }
+        private void DefineVar(BuiltInType aType, IToken aNameToken, IToken userType)
+        {
+            var userDefType = userType?.Text ?? string.Empty;
+            var variable = new VariableSymbol(aNameToken.Text, aType, userDefType);
+            _currentScope.Define(variable);
+        }
 
-      private static IToken GetTypeToken(CaplParser.DirectDeclaratorContext directDeclCtx)
-      {
-         var declToken = directDeclCtx?.Identifier()?.Symbol;
-         while (declToken == null)
-         {
-            directDeclCtx = directDeclCtx?.directDeclarator();
-            declToken = directDeclCtx?.Identifier()?.Symbol;
-         }
+        private void SaveScope(IParseTree aContext, IScope aScope)
+        {
+            Scopes.Put(aContext, aScope);
+        }
 
-         return declToken;
-      }
+        private static IToken GetTypeToken(CaplParser.DirectDeclaratorContext directDeclCtx)
+        {
+            var declToken = directDeclCtx?.Identifier()?.Symbol;
+            while (declToken == null)
+            {
+                directDeclCtx = directDeclCtx?.directDeclarator();
+                declToken = directDeclCtx?.Identifier()?.Symbol;
+            }
 
-      private static CaplParser.TypeSpecifierContext
-         GetTypeSpecContext(IEnumerable<CaplParser.DeclarationSpecifierContext> declSpecCtx) =>
-         declSpecCtx
-            .Select(ctx => ctx.typeSpecifier())
-            .FirstOrDefault(ctx => ctx != null);
-   }
+            return declToken;
+        }
+
+        private static CaplParser.TypeSpecifierContext
+            GetTypeSpecContext(IEnumerable<CaplParser.DeclarationSpecifierContext> declSpecCtx) =>
+            declSpecCtx
+                .Select(ctx => ctx.typeSpecifier())
+                .FirstOrDefault(ctx => ctx != null);
+    }
 }
