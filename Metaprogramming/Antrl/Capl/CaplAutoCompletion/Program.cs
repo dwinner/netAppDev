2d834710190a344f9403fae93bed2778a02f5d3a
vinevtsev <vinevtsev@schleissheimer.de>
2021-12-21 14:56:35 +0100

    Apply validation for CAPL API

--- Metaprogramming/Antrl/Capl/CaplAutoCompletion/Program.cs
+++ Metaprogramming/Antrl/Capl/CaplAutoCompletion/Program.cs
@@ -1,51 +1,25 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Xml;
-using System.Xml.Schema;
 
 namespace CaplAutoCompletion
 {
     internal static class Program
     {
-        private const string CaplApi = "namespacecapl.xml";
-        private const string CaplApiSchema = "namespacecapl.xsd";
-        private static bool _isXmlValid = true;
-
         private static void Main()
         {
-            // Check Capl doc file validation first
-            var settings = new XmlReaderSettings();
-            settings.Schemas.Add(null, CaplApiSchema);
-            settings.ValidationType = ValidationType.Schema;
-            settings.ValidationEventHandler += OnValidation;
-            var reader = XmlReader.Create(CaplApi, settings);
-            while (reader.Read() && _isXmlValid)
+            bool validated;
+            string errorMessage;
+            using (var validator = new XmlValidator("capl_api.xml", "capl_api.xsd"))
             {
-                if (reader.NodeType == XmlNodeType.Text)
-                {
-                    Debug.WriteLine("In validation");
-                }
+                validated = validator.Validate(out errorMessage);
             }
 
-            Console.WriteLine(_isXmlValid ? "XML file is valid" : "XML file is invalid");
-            Console.ReadKey();
-        }
-
-        private static void OnValidation(object sender, ValidationEventArgs e)
-        {
-            Debug.WriteLine(e.Message);
-            switch (e.Severity)
+            if (validated)
             {
-                case XmlSeverityType.Error:
-                    Debug.WriteLine("Error occured: {0}", e.Exception.Message);
-                    _isXmlValid = false;
-                    break;
-                case XmlSeverityType.Warning:
-                    Debug.WriteLine("Warning occured: {0}", e.Message);
-                    _isXmlValid = false;
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
+                Console.WriteLine("Validation is Ok");
+            }
+            else
+            {
+                Console.WriteLine("Validation error: {0}", errorMessage);
             }
         }
     }
