e109663c8c257839b2958a935430ec07f0ec3849
vinevtsev <vinevtsev@schleissheimer.de>
2021-12-21 16:21:12 +0100

    Inner class xPath selection and classification for namespaces

--- Metaprogramming/Antrl/Capl/CaplAutoCompletion/Program.cs
+++ Metaprogramming/Antrl/Capl/CaplAutoCompletion/Program.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.Xml.XPath;
 
 namespace CaplAutoCompletion
 {
     internal static class Program
     {
+        private const string CaplApiXml = "capl_api.xml";
+        private const string CaplApiXsd = "capl_api.xsd";
+
         private static void Main()
         {
             bool validated;
             string errorMessage;
-            using (var validator = new XmlValidator("capl_api.xml", "capl_api.xsd"))
+            using (var validator = new XmlValidator(CaplApiXml, CaplApiXsd))
             {
                 validated = validator.Validate(out errorMessage);
             }
@@ -20,6 +24,18 @@ namespace CaplAutoCompletion
             else
             {
                 Console.WriteLine("Validation error: {0}", errorMessage);
+            }
+
+            var document = new XPathDocument(CaplApiXml);
+            var navigator = ((IXPathNavigable)document).CreateNavigator();
+            if (navigator != null)
+            {
+                var iterator = navigator.Select("/doxygen/compounddef/innerclass");
+                while (iterator.MoveNext())
+                {
+                    var caplClassName = iterator.Current.Value;
+                    Console.WriteLine(caplClassName);
+                }
             }
         }
     }
