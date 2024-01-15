2611ca706b849c788ecfe135650120ffdd7c13a9
Denis Vinevcev <dwinner@inbox.ru>
2024-01-15 09:10:10 +0300

    C#12 up to date

---
+++ CSharp/Strings/SurrogatePairSample/Program.cs
@@ -0,0 +1,11 @@
+﻿const int musicalNote = 0x1D161;
+
+var s = char.ConvertFromUtf32(musicalNote);
+var len = s.Length;
+Console.WriteLine(len);
+
+var s1 = char.ConvertToUtf32(s, 0).ToString("X");
+Console.WriteLine(s1);
+
+var s2 = char.ConvertToUtf32(s[0], s[1]).ToString("X");
+Console.WriteLine(s2);
---
+++ CSharp/Strings/UnicodeUsages/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+var encoding = Encoding.UTF8;
+
+// Call Encoding.GetEncoding with a standard IANA name to obtain an encoding:
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+var chinese = Encoding.GetEncoding("GB18030");
+
+// The static GetEncodings method returns a list of all supported encodings:
+foreach (var info in Encoding.GetEncodings())
+{
+   Console.WriteLine(info.Name);
+}
