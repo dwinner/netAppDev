0807dd8a5e60e524e782a0464055760608357738
Denis Vinevcev <dwinner@inbox.ru>
2025-06-22 21:22:40 +0300

    user of expr.

---
+++ Functional/howtos/UseOfExpr/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Creates a lambda expression `(x, y) => x * y`
+ */
+
+using System.Linq.Expressions;
+
+var baseDamage = Expression.Parameter(typeof(int), "baseDamage");
+var level = Expression.Parameter(typeof(int), "level");
+var multiply = Expression.Multiply(baseDamage, level);
+var damageCalc = Expression.Lambda<Func<int, int, int>>(multiply, baseDamage, level);
+
+// Compile the expression
+var calculateDamage = damageCalc.Compile();
+
+// Calculate tower damage
+var towerDamage = calculateDamage(10, 5);
+Console.WriteLine($"Tower damage: {towerDamage}");
