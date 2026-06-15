e983d17af38fd0334b877549e91cf114ce9fb3c7
Denis Vinevcev <dwinner@inbox.ru>
2026-06-15 18:20:53 +0300

    dev. containers / init

---
+++ containers/EnvironmentLib/EnvironmentInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Runtime.InteropServices;
+
+namespace EnvironmentLib;
+
+public class EnvironmentInfo
+{
+   public string UserName { get; } = Environment.UserName;
+
+   public string HostName { get; } = Dns.GetHostName();
+
+   public string DotNet { get; } = RuntimeInformation.FrameworkDescription;
+
+   public string Os { get; } = RuntimeInformation.OSDescription;
+
+   public string Architecture { get; } = RuntimeInformation.OSArchitecture.ToString();
+
+   public int Processors { get; } = Environment.ProcessorCount;
+
+   public bool InContainer { get; } = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") is not null;
+}
