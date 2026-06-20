b936ac479c17cc04d68adb8650c6bf9ea4d44dc4
Denis Vinevcev <dwinner@inbox.ru>
2026-06-20 08:51:58 +0300

    microservices - host variants

---
+++ Services/micro/host-variants/minimal-APIs/Repositories/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace MyMicroservice.Repositories;
+
+public interface IOrderRepository
+{
+   void DeleteOrder(int id);
+
+   List<Order> GetAllOrders();
+
+   Order? GetOrderById(int id);
+
+   int SaveOrder(Order order);
+}
---
+++ Services/micro/host-variants/minimal-APIs/Usings.cs
@@ -0,0 +1,5 @@
+﻿global using System.ComponentModel.DataAnnotations;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
+global using MyMicroservice.Data;
+global using MyMicroservice.Repositories;
