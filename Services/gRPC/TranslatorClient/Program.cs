bc93a1deae2702d6eb295ac2fb848f3b2331bde2
Denis Vinevcev <dwinner@inbox.ru>
2026-07-03 13:52:25 +0300

    gRPC - wip

---
+++ Services/gRPC/TranslatorClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using Grpc.Net.Client;
+using TranslatorClient;
+
+var words = new List<string>
+{
+   "red", "yellow", "green"
+};
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7279");
+var client = new Translator.TranslatorClient(channel);
+
+words.ForEach(async void (word) =>
+{
+   try
+   {
+      var request = new Request { Word = word };
+      var response = await client.TranslateAsync(request);
+      Console.WriteLine($"{response.Word} : {response.Translation}");
+   }
+   catch (Exception commonEx)
+   {
+      Debug.WriteLine(commonEx.Message);
+   }
+});
+
+Console.ReadKey();
